3a37e7a4a27b86e8ad8e87f2e6822ba48177941f
kedziorno <kjoblak85@gmail.com>
2020-08-22 19:04:31 +0200

    some new

--- test_oled.vhd
+++ test_oled.vhd
@@ -30,20 +30,25 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity test_oled is 
+port (
+signal clk : in std_logic;
+signal sda,scl : inout std_logic
+);
 end test_oled;
 
 architecture Behavioral of test_oled is
 
-signal clk : std_logic;
-signal sda : std_logic;
-signal scl : std_logic;
+constant AMNT_INSTRS: natural := 27;
+type IAR is array (0 to AMNT_INSTRS-1) of std_logic_vector(7 downto 0);
+--signal Instrs: IAR := (x"A8", x"3F", x"D3", x"00", x"40", x"A1", x"DA", x"12", x"81", x"7F", x"20", x"00", x"21", x"00", x"7F", x"22", x"00", x"07", x"A6", x"DB", x"40", x"A4", x"D5", x"80", x"8D", x"14", x"AF");
+signal Instrs : IAR := (x"AE",x"D5",x"80",x"A8",x"3F",x"D3",x"00",x"40",x"8D",x"14",x"20",x"00",x"A0",x"C8",x"DA",x"12",x"81",x"CF",x"D9",x"F1",x"DB",x"40",x"A4",x"A6",x"AF");
 
 SIGNAL i2c_ena     : STD_LOGIC;                     --i2c enable signal
 SIGNAL i2c_addr    : STD_LOGIC_VECTOR(6 DOWNTO 0);  --i2c address signal
 SIGNAL i2c_rw      : STD_LOGIC;                     --i2c read/write command signal
 SIGNAL i2c_data_wr : STD_LOGIC_VECTOR(7 DOWNTO 0);  --i2c write data
 SIGNAL i2c_busy    : STD_LOGIC;                     --i2c busy signal
-SIGNAL i2c_reset    : STD_LOGIC;                     --i2c busy signal
+SIGNAL i2c_reset   : STD_LOGIC;                     --i2c busy signal
 
 COMPONENT i2c IS
 GENERIC(
@@ -65,35 +70,36 @@ END component i2c;
 
 for all : i2c use entity WORK.i2c_master(logic);
 
-procedure clk_gen(signal clk : out std_logic; constant wait_start : time; constant HT : time; constant LT : time) is
-begin
-	clk <= '0';
-	wait for wait_start;
-	loop
-		clk <= '1';
-		wait for HT;
-		clk <= '0';
-		wait for LT;
-	end loop;
-end procedure;
-
 begin
 
-clk_gen(clk,0 ns,20 ns,20 ns);
-	
-c1 : i2c GENERIC MAP(bus_clk => 400_000)
-    PORT MAP(clk => clk, reset_n => i2c_reset, ena => i2c_ena, addr => i2c_addr,
-             rw => i2c_rw, data_wr => i2c_data_wr, busy => i2c_busy,
-             data_rd => open, ack_error => open, sda => sda,
-             scl => scl);
-    process (clk) is
-	begin
-		i2c_reset <= '1';
-		i2c_ena <= '1';
-		i2c_addr <= "1111111";
-		i2c_rw <= '1';
-		i2c_data_wr <= X"FF";
-		
-		end process;
+c1 : i2c 
+GENERIC MAP(bus_clk => 400_000)
+PORT MAP(
+clk => clk,
+reset_n => i2c_reset,
+ena => i2c_ena,
+addr => i2c_addr,
+rw => i2c_rw,
+data_wr => i2c_data_wr,
+busy => i2c_busy,
+data_rd => open,
+ack_error => open,
+sda => sda,
+scl => scl
+);
+
+process (clk) is
+begin
+f0 : for i in Instrs'RANGE loop
+i2c_reset <= '1';
+i2c_ena <= '1';
+i2c_rw <= '1';
+i2c_addr <= X"3C"; -- address 3D 78
+i2c_data_rw <= X"00"; -- control 80
+i2c_data_wr <= Instrs(i); -- command
+i2c_ena <= '0';
+i2c_reset <= '0';
+end loop f0;
+end process;
 end Behavioral;
 
