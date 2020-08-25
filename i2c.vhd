444297ac9fbc82e1af92d25767d9b97ead048043
kedziorno <kjoblak85@gmail.com>
2020-08-25 19:37:32 +0200

    working code

--- i2c.vhd
+++ i2c.vhd
@@ -18,6 +18,37 @@
 --
 ----------------------------------------------------------------------------------
 
+--------------------------------------------------------------------------------
+--
+--   FileName:         i2c_master.vhd
+--   Dependencies:     none
+--   Design Software:  Quartus II 64-bit Version 13.1 Build 162 SJ Full Version
+--
+--   HDL CODE IS PROVIDED "AS IS."  DIGI-KEY EXPRESSLY DISCLAIMS ANY
+--   WARRANTY OF ANY KIND, WHETHER EXPRESS OR IMPLIED, INCLUDING BUT NOT
+--   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+--   PARTICULAR PURPOSE, OR NON-INFRINGEMENT. IN NO EVENT SHALL DIGI-KEY
+--   BE LIABLE FOR ANY INCIDENTAL, SPECIAL, INDIRECT OR CONSEQUENTIAL
+--   DAMAGES, LOST PROFITS OR LOST DATA, HARM TO YOUR EQUIPMENT, COST OF
+--   PROCUREMENT OF SUBSTITUTE GOODS, TECHNOLOGY OR SERVICES, ANY CLAIMS
+--   BY THIRD PARTIES (INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF),
+--   ANY CLAIMS FOR INDEMNITY OR CONTRIBUTION, OR OTHER SIMILAR COSTS.
+--
+--   Version History
+--   Version 1.0 11/01/2012 Scott Larson
+--     Initial Public Release
+--   Version 2.0 06/20/2014 Scott Larson
+--     Added ability to interface with different slaves in the same transaction
+--     Corrected ack_error bug where ack_error went 'Z' instead of '1' on error
+--     Corrected timing of when ack_error signal clears
+--   Version 2.1 10/21/2014 Scott Larson
+--     Replaced gated clock with clock enable
+--     Adjusted timing of SCL during start and stop conditions
+--   Version 2.2 02/05/2015 Scott Larson
+--     Corrected small SDA glitch introduced in version 2.1
+-- 
+--------------------------------------------------------------------------------
+
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 USE ieee.std_logic_unsigned.all;
@@ -230,8 +261,8 @@ BEGIN
                  sda_int WHEN OTHERS;          --set to internal sda signal    
       
   --set scl and sda outputs
-  scl <= '0' WHEN (scl_ena = '1' AND scl_clk = '0') ELSE '1';
-  sda <= '0' WHEN sda_ena_n = '0' ELSE '1';
+  scl <= '0' WHEN (scl_ena = '1' AND scl_clk = '0') ELSE 'Z';
+  sda <= '0' WHEN sda_ena_n = '0' ELSE 'Z';
   
 END logic;
 
