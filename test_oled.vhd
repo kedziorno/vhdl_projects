08db670ff8cf44e58841e13845a5c26636cbaf39
kedziorno <kjoblak85@gmail.com>
2020-09-02 18:49:09 +0200

    changes

--- test_oled.vhd
+++ test_oled.vhd
@@ -36,6 +36,9 @@ end test_oled;
 
 architecture Behavioral of test_oled is
 
+constant GCLK : integer := 50_000_000;
+constant BCLK : integer := 100_000;
+
 constant OLED_WIDTH : integer := 128;
 constant OLED_HEIGHT : integer := 32;
 constant OLED_PAGES : integer := (OLED_HEIGHT / 8);
@@ -43,102 +46,55 @@ constant OLED_PAGES_ALL : integer := OLED_WIDTH * OLED_PAGES;
 constant SSD1306_DATA : integer := to_integer(unsigned'(x"40"));
 constant SSD1306_COMMAND : integer := to_integer(unsigned'(x"00"));
 
-shared variable busy_cnt : INTEGER := 0;
-shared variable v_ff : std_logic_vector(7 downto 0) := "11111111";
+SIGNAL busy_cnt : INTEGER := 0;
 shared variable a,b : integer := 0;
 
---constant NI_INIT : natural := 17;
---type A_INIT is array (0 to NI_INIT-1) of std_logic_vector(7 downto 0);
---signal init_display : A_INIT :=
---(
---	x"A8",
---	x"3F",
---
---	x"D3",
---	x"00",
---
---	x"A1",
---
---	x"C8",
---
---	x"DA",
---	x"02",
---
---	x"81",
---	x"FF",
---
---	x"A4",
---
---	x"A6",
---
---	x"D5",
---	x"80",
---
---	x"8D",
---	x"14",
---
---	x"AF"
---);
-
-constant NI_INIT : natural := 17;
+constant NI_INIT : natural := 27;
 type A_INIT is array (0 to NI_INIT-1) of std_logic_vector(7 downto 0);
 signal init_display : A_INIT :=
 (
---	x"00", -- ssd1306_command
-
-	x"A8",
-	x"3F",
-
-	x"D3",
-	x"00",
-
---	x"40",
-
-	x"A1",
-
-	x"C8",
-
-	x"DA",
-	x"02",
-
-	x"81",
-	x"7F",
-
-	x"A4",
-
-	x"A6",
-
+	x"AE",
 	x"D5",
 	x"80",
-
+	x"A8",
+	x"3F",
+	x"D3",
+	x"00",
+	x"40",
 	x"8D",
 	x"14",
-
---	x"db",
---	x"40",
---
---	x"d9",
---	x"f1",
---
---	x"20",
---	x"00",
-
-	x"AF"
+	x"20",
+	x"00",
+	x"A1",
+	x"C8",
+	x"DA",
+	x"02",
+	x"81",
+	x"30",
+	x"d9",
+	x"f1",
+	x"db",
+	x"40",
+	x"A4",
+	x"A6",
+	x"2e",
+	x"AF",
+	x"A5"
 );
 
-constant NI_CLEAR : natural := 7;
+constant NI_CLEAR : natural := 6;
 type A_CLEAR is array (0 to NI_CLEAR-1) of std_logic_vector(7 downto 0);
 
 signal clear_display : A_CLEAR :=
 (
-	std_logic_vector(to_unsigned(SSD1306_DATA,8)), --
+	--std_logic_vector(to_unsigned(SSD1306_DATA,8)), --
 
-	x"21", -- 
-	x"00", -- 
+	x"21", --
+	x"00", --
 	std_logic_vector(to_unsigned(OLED_WIDTH-1,8)), -- 
 
-	x"22", -- 
-	x"00", -- 
+	x"22", --
+	x"00", --
 	std_logic_vector(to_unsigned(OLED_PAGES-1,8))  --
 );
 
@@ -153,7 +109,8 @@ SIGNAL busy_prev   : STD_LOGIC;                     --previous value of i2c busy
 COMPONENT i2c IS
 GENERIC(
 input_clk : INTEGER := 50_000_000; --input clock speed from user logic in Hz
-bus_clk   : INTEGER := 400_000);   --speed the i2c bus (scl) will run at in Hz
+bus_clk   : INTEGER := 100_000 --speed the i2c bus (scl) will run at in Hz
+);
 PORT(
 clk       : IN     STD_LOGIC;                    --system clock
 reset_n   : IN     STD_LOGIC;                    --active low reset
@@ -172,22 +129,24 @@ for all : i2c use entity WORK.i2c_master(logic);
 
 type state is 
 (
+--	waiting,
 	start,
 	set_address,
 	clear_display_state,
 	stop
 );
 signal c_state,n_state : state := start;
-
-shared variable idx_i : integer := 0;
-
+	
 begin
 
---clk_gen(clk,0 ns,20 ns,20 ns);
-
 c1 : i2c
-GENERIC MAP(bus_clk => 400_000)
-PORT MAP(
+GENERIC MAP
+(
+input_clk => GCLK,
+bus_clk => BCLK
+)
+PORT MAP
+(
 clk => clk,
 reset_n => i2c_reset,
 ena => i2c_ena,
@@ -202,79 +161,132 @@ scl => scl
 );
 
 p0 : process (clk,i2c_reset) is
+variable counter : INTEGER RANGE 0 TO GCLK/10 := 0; --counts 100ms to wait before communicating
 begin
 IF(i2c_reset='0') THEN
 i2c_ena <= '0';
-busy_cnt := 0;
+busy_cnt <= 0;
 c_state <= start;
-i2c_reset <= '1';
 elsif(rising_edge(clk)) then
 c_state <= n_state;
 case c_state is
+--	WHEN waiting =>
+--		IF(counter < GCLK/10) THEN --100ms not yet reached
+--			counter := counter + 1; --increment counter
+--			n_state <= waiting;
+--		ELSE --100ms reached
+--			counter := 0; --clear counter
+--			n_state <= start; --advance to setting the gain
+--		END IF;
+
 	when start =>
 		busy_prev <= i2c_busy;
 		if(busy_prev='0' and i2c_busy='1') then
-			busy_cnt := busy_cnt + 1;
+			busy_cnt <= busy_cnt + 1;
 		end if;
-		case busy_cnt is
-			when 0 =>
-				i2c_ena <= '1'; -- we are busy
-				i2c_addr <= "0111100"; -- address 3C 3D 78 ; 0111100 0111101 1111000
-				i2c_rw <= '0';
-			when 1 to NI_INIT =>
-				i2c_data_wr <= init_display(busy_cnt-1); -- command
-			when NI_INIT+1 =>
-				i2c_ena <= '0';
-				if(i2c_busy='0') then
-					busy_cnt := 0;
-					n_state <= set_address;
-				end if;
-			when others => null;
-		end case;
-	when set_address =>
-		busy_prev <= i2c_busy;
-		if(busy_prev='0' and i2c_busy='1') then
-			busy_cnt := busy_cnt + 1;
+		if (busy_cnt=0) then
+			i2c_reset <= '1';
+			i2c_ena <= '1'; -- we are busy
+			i2c_addr <= "0111100"; -- address 3C 3D 78 ; 0111100 0111101 1111000
+			i2c_rw <= '0';
 		end if;
-		case busy_cnt is
-			when 0 =>
-				i2c_ena <= '1'; -- we are busy
-				i2c_addr <= "0111100"; -- address 3C 3D 78 ; 0111100 0111101 1111000
-				i2c_rw <= '0';
-			when 1 to NI_CLEAR =>
-				i2c_data_wr <= clear_display(busy_cnt-1); -- command
-			when NI_CLEAR+1 =>
-				i2c_ena <= '0';
-				if(i2c_busy='0') then
-					busy_cnt := 0;
-					n_state <= clear_display_state;
-				end if;
-			when others => null;
-		end case;
-	when clear_display_state =>
-		busy_prev <= i2c_busy;
-		if(busy_prev='0' and i2c_busy='1') then
-			busy_cnt := busy_cnt + 1;
+		if ((busy_cnt/=0) and (busy_cnt<(NI_INIT*2)-1) and ((busy_cnt mod 2) = 1)) then
+			i2c_data_wr <= x"00";
 		end if;
-		case busy_cnt is
-			when 0 =>
-				i2c_ena <= '1'; -- we are busy
-				i2c_addr <= "0111100"; -- address 3C 3D 78 ; 0111100 0111101 1111000
-				i2c_rw <= '0';
-			when 1 =>
-				i2c_data_wr <= std_logic_vector(to_unsigned(SSD1306_DATA,8));
-			when 2 to OLED_PAGES_ALL =>
-				i2c_data_wr <= x"00";
-			when OLED_PAGES_ALL+1 =>
-				i2c_ena <= '0';
-				if(i2c_busy='0') then
-					busy_cnt := 0;
-					n_state <= stop;
-				end if;
-			when others => null;
-		end case;
-	when stop =>
-		n_state <= stop;
+		if ((busy_cnt/=0) and (busy_cnt<(NI_INIT*2)-1) and ((busy_cnt mod 2) = 0)) then
+			i2c_data_wr <= init_display((busy_cnt / 2)-1); -- command
+		end if;
+		if ((busy_cnt/=0) and busy_cnt=(NI_INIT*2)-1) then
+			i2c_ena <= '0';
+			if(i2c_busy='0') then
+				busy_cnt <= 0;
+				n_state <= stop;
+			end if;
+		end if;
+
+--		case busy_cnt is
+--			when 0 =>
+--				i2c_ena <= '1'; -- we are busy
+--				i2c_addr <= "0111100"; -- address 3C 3D 78 ; 0111100 0111101 1111000
+--				i2c_rw <= '0';
+--			when 1 to NI_INIT =>
+--				i2c_data_wr <= x"00";
+--				i2c_data_wr <= init_display(busy_cnt-1); -- command
+--			when NI_INIT+1 =>
+--				i2c_ena <= '0';
+--				if(i2c_busy='0') then
+--					busy_cnt := 0;
+--					n_state <= set_address;
+--				end if;
+--			when others => null;
+--		end case;
+
+--	when set_address =>
+--		busy_prev <= i2c_busy;
+--		if(busy_prev='0' and i2c_busy='1') then
+--			busy_cnt := busy_cnt + 1;
+--		end if;
+--		if (busy_cnt=0) then
+--			i2c_ena <= '1'; -- we are busy
+--			i2c_addr <= "0111100"; -- address 3C 3D 78 ; 0111100 0111101 1111000
+--			i2c_rw <= '0';
+--		end if;
+--		if ((busy_cnt<(NI_CLEAR*2)-1) and ((busy_cnt mod 2) = 1)) then
+--			i2c_data_wr <= x"00";
+--		end if;
+--		if ((busy_cnt<(NI_CLEAR*2)-1) and ((busy_cnt mod 2) = 0)) then
+--			i2c_data_wr <= clear_display((busy_cnt / 2)); -- command
+--		end if;
+--		if (busy_cnt=(NI_CLEAR*2)-1) then
+--			i2c_ena <= '0';
+--			if(i2c_busy='0') then
+--				busy_cnt := 0;
+--				n_state <= clear_display_state;
+--			end if;
+--		end if;
+
+--		case busy_cnt is
+--			when 0 =>
+--				i2c_ena <= '1'; -- we are busy
+--				i2c_addr <= "0111100"; -- address 3C 3D 78 ; 0111100 0111101 1111000
+--				i2c_rw <= '0';
+--			when 1 to NI_CLEAR =>
+--				i2c_data_wr <= clear_display(busy_cnt-1); -- command
+--			when NI_CLEAR+1 =>
+--				i2c_ena <= '0';
+--				if(i2c_busy='0') then
+--					busy_cnt := 0;
+--					n_state <= clear_display_state;
+--				end if;
+--			when others => null;
+--		end case;
+
+--	when clear_display_state =>
+--		busy_prev <= i2c_busy;
+--		if(busy_prev='0' and i2c_busy='1') then
+--			busy_cnt := busy_cnt + 1;
+--		end if;
+--		case busy_cnt is
+--			when 0 =>
+--				i2c_ena <= '1'; -- we are busy
+--				i2c_addr <= "0111100"; -- address 3C 3D 78 ; 0111100 0111101 1111000
+--				i2c_rw <= '0';
+--			when 1 =>
+--				i2c_data_wr <= std_logic_vector(to_unsigned(SSD1306_DATA,8));
+--			when 2 to OLED_PAGES_ALL =>
+--				i2c_data_wr <= x"00";
+--			when OLED_PAGES_ALL+1 =>
+--				i2c_ena <= '0';
+--				if(i2c_busy='0') then
+--					busy_cnt := 0;
+--					n_state <= stop;
+--				end if;
+--			when others => null;
+--		end case;
+
+--	when stop =>
+--		n_state <= waiting;
+
 	when others => null;
 end case;
 end if;
