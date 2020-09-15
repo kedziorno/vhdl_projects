bc9140f029e539fc79e4e60b5aa1750ea747c68f
kedziorno <kjoblak85@gmail.com>
2020-09-15 15:31:38 +0200

    add debounce_button and tb but not work

---
+++ debounce_button.vhd
@@ -0,0 +1,92 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    14:13:46 09/15/2020 
+-- Design Name: 
+-- Module Name:    debounce_button - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity debounce_button is
+Port
+(
+i_button : in  STD_LOGIC;
+i_clk : in  STD_LOGIC;
+o_stable : out  STD_LOGIC
+);
+end debounce_button;
+
+architecture Behavioral of debounce_button is
+
+COMPONENT FF_D_GATED_NAND
+PORT(
+D : IN  std_logic;
+E : IN  std_logic;
+Q1 : INOUT  std_logic;
+Q2 : INOUT  std_logic
+);
+END COMPONENT;
+for all : FF_D_GATED_NAND use entity WORK.FF_D_GATED(Behavioral_GATED_D_NOR);
+
+component GAND is
+port (A,B:in STD_LOGIC;C:out STD_LOGIC);
+end component GAND;
+for all : GAND use entity WORK.GATE_AND(GATE_AND_BEHAVIORAL_1);
+
+component GOR is
+port (A,B:in STD_LOGIC;C:out STD_LOGIC);
+end component GOR;
+for all : GOR use entity WORK.GATE_OR(GATE_OR_BEHAVIORAL_1);
+
+signal sa,sb,sc,sd,se,sf,sg,sh : std_logic;
+
+begin
+
+uut1: FF_D_GATED_NAND PORT MAP (
+D => i_button,
+E => i_clk,
+Q1 => sa,
+Q2 => open
+);
+
+uut2: FF_D_GATED_NAND PORT MAP (
+D => sa,
+E => i_clk,
+Q1 => sb,
+Q2 => open
+);
+
+uut3: FF_D_GATED_NAND PORT MAP (
+D => sb,
+E => i_clk,
+Q1 => open,
+Q2 => sc
+);
+
+g1: GOR port map (sa,sb,sd);
+g2: GAND port map (sd,sc,o_stable);
+
+end Behavioral;
+
---
+++ tb_debounce_button.vhd
@@ -0,0 +1,96 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   14:32:21 09/15/2020
+-- Design Name:   
+-- Module Name:   /home/user/workspace/i2c_test_2/tb_debounce_button.vhd
+-- Project Name:  i2c_test_1
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: debounce_button
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+USE ieee.numeric_std.ALL;
+ 
+ENTITY tb_debounce_button IS
+END tb_debounce_button;
+ 
+ARCHITECTURE behavior OF tb_debounce_button IS 
+
+procedure clk_gen(signal clk : out std_logic; constant wait_start : time; constant HT : time; constant LT : time) is
+begin
+clk <= '0';
+wait for wait_start;
+loop
+clk <= '1';
+wait for HT;
+clk <= '0';
+wait for LT;
+end loop;
+end procedure;
+
+COMPONENT debounce_button
+PORT(
+i_button : IN  std_logic;
+i_clk : IN  std_logic;
+o_stable : OUT  std_logic
+);
+END COMPONENT;
+
+signal i_button : std_logic := '0';
+signal i_clk : std_logic := '0';
+signal o_stable : std_logic := '0';
+
+BEGIN
+
+clk_gen(i_clk, 10 ns, 20 ns, 20 ns);
+
+uut: debounce_button PORT MAP (
+i_button => i_button,
+i_clk => i_clk,
+o_stable => o_stable
+);
+
+-- Stimulus process
+stim_proc: process
+begin
+
+i_button <= '0';
+wait for 500 ns;
+i_button <= '1';
+wait for 130 ns;
+i_button <= '0';
+wait for 40 ns;
+i_button <= '1';
+wait for 60 ns;
+i_button <= '0';
+wait for 30 ns;
+i_button <= '1';
+wait for 1200 ns;
+i_button <= '0';
+wait for 500 ns;
+
+wait;
+end process;
+
+END;
