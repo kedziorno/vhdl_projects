a893711bfc0d53653609fd6e54c9c4556715339d
kedziorno <kjoblak85@gmail.com>
2020-09-15 15:55:14 +0200

    add delay

--- gate_not.vhd
+++ gate_not.vhd
@@ -2,10 +2,11 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity GATE_NOT is
+generic (delay:TIME := 0 ns);
 port (A:in STD_LOGIC;B:out STD_LOGIC);
 end entity GATE_NOT;
 
 architecture GATE_NOT_BEHAVIORAL_1 of GATE_NOT is
 begin
-B <= not A;
+B <= not A after delay;
 end architecture GATE_NOT_BEHAVIORAL_1;
