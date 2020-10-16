042068e165931c04416499379e31e45e4e8255fe
kedziorno <kjoblak85@gmail.com>
2020-10-16 12:54:39 +0200

    something new in grandmother

--- debounce_button.vhd
+++ debounce_button.vhd
@@ -41,78 +41,56 @@ end debounce_button;
 
 architecture Behavioral of debounce_button is
 
-component clock_divider is
-Generic(g_board_clock : integer);
-Port(
-i_clk : in  STD_LOGIC;
-o_clk_25khz : out  STD_LOGIC;
-o_clk_50khz : out  STD_LOGIC
-);
-end component clock_divider;
-for all : clock_divider use entity WORK.clock_divider(Behavioral);
-
-COMPONENT FF_D_GATED_NAND
-GENERIC(
-DELAY_AND : TIME;
-DELAY_OR : TIME;
-DELAY_NOT : TIME
-);
-PORT(
-D : IN  std_logic;
-E : IN  std_logic;
-Q1 : INOUT  std_logic;
-Q2 : INOUT  std_logic
-);
-END COMPONENT;
-for all : FF_D_GATED_NAND use entity WORK.FF_D_GATED(Behavioral_GATED_D_NAND);
-
-component GAND is
-GENERIC (
-DELAY_AND : TIME
-);
-port (
-A,B : in STD_LOGIC;
-C : out STD_LOGIC
-);
-end component GAND;
-for all : GAND use entity WORK.GATE_AND(GATE_AND_BEHAVIORAL_1);
-
-signal sa,sb,sc,sd,se,sf,sg,sh : std_logic := '0';
-
-signal clk_25khz : std_logic;
-signal clk_50khz : std_logic;
-
-constant delay_and : TIME := 0 ns;
-constant delay_or : TIME := 0 ns;
-constant delay_not : TIME := 0 ns;
+signal slow_clk_en : std_logic;
+signal Q0,Q1,Q2,Q2_bar : std_logic := '0';
 
 begin
 
-clk_div : clock_divider
-generic map (
-g_board_clock => g_board_clock
-)
-port map
-(
-i_clk => i_clk,
-o_clk_25khz => clk_25khz,
-o_clk_50khz => clk_50khz
-);
+p0 : process (i_clk) is
+	variable aa : integer := 250_000;
+	variable counter : integer := 0;
+	variable clk : std_logic;
+begin
+	if (rising_edge(i_clk)) then
+		if (counter = aa-1) then
+			counter := 0;
+			clk := '1';			
+		else
+			clk := '0';
+		end if;
+	end if;
+	counter := counter + 1;
+	slow_clk_en <= clk;
+end process p0;
 
-uut1: FF_D_GATED_NAND
-GENERIC MAP (DELAY_AND => delay_and,DELAY_OR => delay_or,DELAY_NOT => delay_not)
-PORT MAP (D => i_button,E => i_clk,Q1 => sa,Q2 => open);
+p1a : process (i_clk) is
+begin
+	if (rising_edge(i_clk)) then
+		--if (slow_clk_en = '1') then
+			Q0 <= i_button;
+		--end if;
+	end if;
+end process p1a;
 
-uut2: FF_D_GATED_NAND
-GENERIC MAP (DELAY_AND => delay_and,DELAY_OR => delay_or,DELAY_NOT => delay_not)
-PORT MAP (D => sa,E => i_clk,Q1 => sb,Q2 => open);
+p1b : process (i_clk) is
+begin
+	if (rising_edge(i_clk)) then
+		--if (slow_clk_en = '1') then
+			Q1 <= Q0;
+		--end if;
+	end if;
+end process p1b;
 
-uut3: FF_D_GATED_NAND
-GENERIC MAP (DELAY_AND => delay_and,DELAY_OR => delay_or,DELAY_NOT => delay_not)
-PORT MAP (D => sb,E => i_clk,Q1 => open,Q2 => sc);
+p1c : process (i_clk) is
+begin
+	if (rising_edge(i_clk)) then
+		--if (slow_clk_en = '1') then
+			Q2 <= Q1;
+		--end if;
+	end if;
+end process p1c;
 
-g1: GAND GENERIC MAP (DELAY_AND => delay_and) port map (sa,sb,sd);
-g2: GAND GENERIC MAP (DELAY_AND => delay_and) port map (sd,sc,o_stable);
+Q2_bar <= not Q2;
+o_stable <= Q1 and Q2_bar;
 
 end Behavioral;
-
