15a3f4e2430463f0cf983b0c1a4407cdb91c4aa5
kedziorno <kjoblak85@gmail.com>
2020-11-09 20:30:53 +0100

    add rest files , fix rebase conflicts

--- clock_divider.vhd
+++ clock_divider.vhd
@@ -34,20 +34,24 @@ Generic(g_board_clock : integer);
 Port(
 i_clk : in  STD_LOGIC;
 o_clk_25khz : out  STD_LOGIC;
-o_clk_50khz : out  STD_LOGIC
+o_clk_50khz : out  STD_LOGIC;
+o_clk_1second : out  STD_LOGIC
 );
 end clock_divider;
 
 architecture Behavioral of clock_divider is
-	constant clk25khz_div : integer := g_board_clock / 25_000;
-	constant clk50khz_div : integer := g_board_clock / 50_000;
+	constant clk25khz_div   : integer := g_board_clock / 25_000;
+	constant clk50khz_div   : integer := g_board_clock / 50_000;
+	constant clk1second_div : integer := g_board_clock;
 begin
 
 p0 : process (i_clk) is
 	variable clk_out25 : std_logic;
 	variable clk_out50 : std_logic;
+	variable clk_out1s : std_logic;
 	variable counter1 : integer := 0;
 	variable counter2 : integer := 0;
+	variable counter3 : integer := 0;
 begin
 	if (rising_edge(i_clk)) then
 		if (counter1 = clk25khz_div-1) then
@@ -64,9 +68,17 @@ begin
 			clk_out50 := '0';
 			counter2 := counter2 + 1;
 		end if;
+		if (counter3 = clk1second_div-1) then
+			clk_out1s := '1';
+			counter3 := 0;
+		else
+			clk_out1s := '0';
+			counter3 := counter3 + 1;
+		end if;
 	end if;
 	o_clk_25khz <= clk_out25;
 	o_clk_50khz <= clk_out50;
+	o_clk_1second <= clk_out1s;
 end process p0;
 
 end Behavioral;
--- myown_i2c/top.vhd
+++ myown_i2c/top.vhd
@@ -2,11 +2,7 @@
 -- Company: 
 -- Engineer: 
 -- 
-<<<<<<< HEAD
 -- Create Date:    20:56:44 09/07/2020 
-=======
--- Create Date:    22:11:54 09/04/2020 
->>>>>>> add rest files , fix rebase conflicts
 -- Design Name: 
 -- Module Name:    top - Behavioral 
 -- Project Name: 
@@ -23,18 +19,11 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-<<<<<<< HEAD
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-=======
 use WORK.p_pkg1.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 use IEEE.NUMERIC_STD.ALL;
->>>>>>> add rest files , fix rebase conflicts
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx primitives in this code.
@@ -42,82 +31,88 @@ use IEEE.NUMERIC_STD.ALL;
 --use UNISIM.VComponents.all;
 
 entity top is
-<<<<<<< HEAD
 Port(
 clk : in STD_LOGIC;
 rst : in STD_LOGIC;
 sda : out STD_LOGIC;
 sck : out STD_LOGIC
-=======
-port(
-signal clk : in std_logic;
-signal btn_1 : in std_logic;
-signal sda,scl : inout std_logic
->>>>>>> add rest files , fix rebase conflicts
 );
 end top;
 
 architecture Behavioral of top is
 
-<<<<<<< HEAD
-component power_on is 
-port
-(
-	signal clk,rst : in std_logic;
-	signal sda,sck : out std_logic
-);
-end component power_on;
+constant INPUT_CLOCK : integer := 50_000_000;
+constant BUS_CLOCK : integer := 100_000;
+constant OLED_WIDTH : integer := 128;
+constant OLED_HEIGHT : integer := 32;
 
-for all : power_on use entity WORK.power_on(Behavioral);
-
-begin
-
-c0 : power_on
-port map
-(
-	clk => clk,
-	rst => rst,
-	sda => sda,
-	sck => sck
-);
-
-end Behavioral;
-=======
-component test_oled is 
-port
-(
+component oled_display is
+generic(
+GLOBAL_CLK : integer;
+I2C_CLK : integer;
+WIDTH : integer;
+HEIGHT : integer);
+port(
 signal i_clk : in std_logic;
 signal i_rst : in std_logic;
-signal i_char : in array1;
-signal io_sda,io_scl : inout std_logic
+signal i_x : in integer;
+signal i_y : in integer;
+signal i_data : in std_logic;
+signal io_sda,io_scl : inout std_logic);
+end component oled_display;
+for all : oled_display use entity WORK.oled_display(Behavioral);
+
+component clock_divider is
+Generic(g_board_clock : integer);
+Port(
+i_clk : in  STD_LOGIC;
+o_clk_25khz : out  STD_LOGIC;
+o_clk_50khz : out  STD_LOGIC;
+o_clk_1second : out  STD_LOGIC
 );
-end component test_oled;
+end component clock_divider;
+for all : clock_divider use entity WORK.clock_divider(Behavioral);
 
-for all : test_oled use entity WORK.test_oled(Behavioral);
-
-constant TEXT_LENGTH : integer := 26;
-signal font_character : array1(0 to TEXT_LENGTH-1);
-signal text : array1(0 to TEXT_LENGTH-1) := (x"4C",x"6F",x"52",x"65",x"4D",x"20",x"49",x"70",x"53",x"75",x"4D",x"20",x"64",x"4F",x"6C",x"4F",x"72",x"20",x"73",x"49",x"74",x"20",x"41",x"6D",x"45",x"74"); -- Lorem ipsum dolor sit amet
+signal a,b : integer := 0;
+signal rst : std_logic := '0';
+signal clk_1s : std_logic := '0';
 
 begin
 
-c0 : test_oled
-port map
-(
+clk_div : clock_divider
+generic map(g_board_clock => INPUT_CLOCK)
+port map(
+i_clk => clk,
+o_clk_25khz => open,
+o_clk_50khz => open,
+o_clk_1second => clk_1s);
+
+c0 : oled_display
+generic map(
+	GLOBAL_CLK => INPUT_CLOCK,
+	I2C_CLK => BUS_CLOCK,
+	WIDTH => OLED_WIDTH,
+	HEIGHT => OLED_HEIGHT)
+port map(
 	i_clk => clk,
 	i_rst => btn_1,
-	i_char => font_character,
+	i_x => a,
+	i_y => b,
+	i_data => '1',
 	io_sda => sda,
 	io_scl => scl
 );
 
-p0 : process (clk) is
+p0 : process (clk_1s) is
 begin
-	if (rising_edge(clk)) then
-		font_character <= text;
+	if (btn_1 = '1') then
+		a <= 0;
+		b <= 0;
+	elsif (rising_edge(clk_1s)) then
+		a <= a + 1;
+		b <= b + 1;
 	end if;
 end process p0;
 
 end Behavioral;
 
->>>>>>> add rest files , fix rebase conflicts
