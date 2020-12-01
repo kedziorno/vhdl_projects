41145e961c362e6022aaad3a0a1161b438c2b8b9
kedziorno <kjoblak85@gmail.com>
2020-12-01 21:04:15 +0100

    update synthesis script and prj files

--- gof/synthesis-top.sh
+++ gof/synthesis-top.sh
@@ -7,40 +7,42 @@ if [ $? -ne 0 ];
 then
 	echo "error on xst";
 	exit;
-fi
-
-ngdbuild -intstyle ise -dd _ngo -nt timestamp -uc Nexys2_1200General.ucf -p xc3s1200e-fg320-4 top.ngc top.ngd
-if [ $? -ne 0 ];
-then
-	echo "error on ngdbuild";
-	exit;
-fi
-
-map -intstyle ise -p xc3s1200e-fg320-4 -ol std -timing -cm balanced -ir off -pr off -o top_map.ncd top.ngd top.pcf
-if [ $? -ne 0 ];
-then
-	echo "error on map";
-	exit;
-fi
-
-par -w -intstyle ise -ol std -rl std -t 1 top_map.ncd top.ncd top.pcf
-if [ $? -ne 0 ];
-then
-	echo "error on par";
-	exit;
-fi
-
-trce -intstyle ise -v 3 -s 4 -n 3 -fastpaths -xml top.twx top.ncd -o top.twr top.pcf -ucf Nexys2_1200General.ucf
-if [ $? -ne 0 ];
-then
-	echo "error on trce";
-	exit;
-fi
-
-bitgen -intstyle ise -f top.ut top.ncd
-if [ $? -ne 0 ];
-then
-	echo "error on bitgen";
-	exit;
+else
+	ngdbuild -intstyle ise -dd _ngo -nt timestamp -uc Nexys2_1200General.ucf -p xc3s1200e-fg320-4 top.ngc top.ngd
+	if [ $? -ne 0 ];
+	then
+		echo "error on ngdbuild";
+		exit;
+	else
+		map -intstyle ise -p xc3s1200e-fg320-4 -ol std -timing -cm balanced -ir off -pr off -o top_map.ncd top.ngd top.pcf
+		if [ $? -ne 0 ];
+		then
+			echo "error on map";
+			exit;
+		else
+			par -w -intstyle ise -ol std -rl std -t 1 top_map.ncd top.ncd top.pcf
+			if [ $? -ne 0 ];
+			then
+				echo "error on par";
+				exit;
+			else
+				trce -intstyle ise -v 3 -s 4 -n 3 -fastpaths -xml top.twx top.ncd -o top.twr top.pcf -ucf Nexys2_1200General.ucf
+				if [ $? -ne 0 ];
+				then
+					echo "error on trce";
+					exit;
+				else
+					bitgen -intstyle ise -f top.ut top.ncd
+					if [ $? -ne 0 ];
+					then
+						echo "error on bitgen";
+						exit;
+					else
+						ls -l top.bit
+					fi
+				fi
+			fi
+		fi
+	fi
 fi
 
