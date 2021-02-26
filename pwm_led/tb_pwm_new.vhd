168d55e496964ed1fe4e9295b908620c2ac63252
kedziorno <kjoblak85@gmail.com>
2021-02-26 20:38:59 +0100

    test3

--- pwm_led/tb_pwm_new.vhd
+++ pwm_led/tb_pwm_new.vhd
@@ -60,7 +60,7 @@ ARCHITECTURE behavior OF tb_pwm_new IS
    signal o_pwm : std_logic;
 
    -- Clock period definitions
-   constant i_clock_period : time := 10 ns;
+   constant i_clock_period : time := 20 ns;
  
 BEGIN
  
@@ -84,60 +84,58 @@ BEGIN
 
    -- Stimulus process
    stim_proc: process
+		variable wait_pwm : integer := 20;
    begin		
       -- hold reset state for 100 ns.
 			i_reset <= '1';
-      wait for 100 ns;	
+      wait for i_clock_period;	
 			i_reset <= '0';
 
-      wait for i_clock_period*10;
-
 			i_data <= 15;
-			wait for i_clock_period*10;
+			wait for i_clock_period*wait_pwm;
 
 			i_data <= 14;
-			wait for i_clock_period*10;
+			wait for i_clock_period*wait_pwm;
 
 			i_data <= 13;
-			wait for i_clock_period*10;
+			wait for i_clock_period*wait_pwm;
 
 			i_data <= 12;
-			wait for i_clock_period*10;
+			wait for i_clock_period*wait_pwm;
 
 			i_data <= 11;
-			wait for i_clock_period*10;
+			wait for i_clock_period*wait_pwm;
 
 			i_data <= 10;
-			wait for i_clock_period*10;
+			wait for i_clock_period*wait_pwm;
 
 			i_data <= 9;
-			wait for i_clock_period*10;
+			wait for i_clock_period*wait_pwm;
 
 			i_data <= 8;
-			wait for i_clock_period*10;
+			wait for i_clock_period*wait_pwm;
 
 			i_data <= 7;
-			wait for i_clock_period*10;
+			wait for i_clock_period*wait_pwm;
 
 			i_data <= 6;
-			wait for i_clock_period*10;
+			wait for i_clock_period*wait_pwm;
 
 			i_data <= 5;
-			wait for i_clock_period*10;
+			wait for i_clock_period*wait_pwm;
 
 			i_data <= 4;
-			wait for i_clock_period*10;
+			wait for i_clock_period*wait_pwm;
 
 			i_data <= 3;
-			wait for i_clock_period*10;
+			wait for i_clock_period*wait_pwm;
 
 			i_data <= 2;
-			wait for i_clock_period*10;
+			wait for i_clock_period*wait_pwm;
 
 			i_data <= 1;
-			wait for i_clock_period*10;
+			wait for i_clock_period*wait_pwm;
 
-			wait;
    end process;
 
 END;
