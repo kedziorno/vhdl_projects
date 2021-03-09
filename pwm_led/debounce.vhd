697c72b9f93fe1163a6f2a0471ad2da92a9240a1
kedziorno <kjoblak85@gmail.com>
2021-03-09 13:40:57 +0100

    test1 - debounce code,btn1/2 - discrete low and high without range

--- pwm_led/debounce.vhd
+++ pwm_led/debounce.vhd
@@ -30,15 +30,53 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity debounce is
+Generic (
+	G_BOARD_CLOCK : integer
+);
 Port (
-clk : in  STD_LOGIC;
-i_btn : in  STD_LOGIC;
-o_db_btn : out  STD_LOGIC
+	i_clk : in  STD_LOGIC;
+	i_btn : in  STD_LOGIC;
+	o_db_btn : out  STD_LOGIC
 );
 end debounce;
 
 architecture Behavioral of debounce is
 
+	COMPONENT clock_divider_cnt IS
+	Generic (
+		g_board_clock : integer;
+		g_divider : integer
+	);
+	Port (
+		i_clock : in STD_LOGIC;
+		o_clock : out STD_LOGIC
+	);
+	END COMPONENT clock_divider_cnt;
+
+	signal d_clk : std_logic;
+	signal q0,q1,q2 : std_logic;
+
 begin
+
+	clk_div_cnt : clock_divider_cnt
+	GENERIC MAP (
+		g_board_clock => G_BOARD_CLOCK,
+		g_divider => 33 -- XXX ~30ms
+	)
+	PORT MAP (
+		i_clock => i_clk,
+		o_clock => d_clk
+	);
+	
+	p0 : process (d_clk) is
+	begin
+		if (rising_edge(d_clk)) then
+			q2 <= i_btn;
+			q1 <= q2;
+			q0 <= q1;
+		end if;
+	end process p0;
+
+	o_db_btn <= (q2 and q1 and not q0);
 
 end Behavioral;
--- pwm_led/top.vhd
+++ pwm_led/top.vhd
@@ -61,36 +61,104 @@ architecture Behavioral of top is
 	);
 	END COMPONENT PWM_NEW;
 
+	COMPONENT debounce IS
+	Generic (
+		G_BOARD_CLOCK : integer
+	);
+	Port (
+		i_clk : in  STD_LOGIC;
+		i_btn : in  STD_LOGIC;
+		o_db_btn : out  STD_LOGIC
+	);
+	END COMPONENT debounce;
+
 	constant PWM_RES : integer := PWM_WIDTH;
 	constant L_DATA	: integer range 0 to LEDS-1 := LEDS-1;
 	type A_DATA is array(0 to L_DATA) of INTEGER RANGE 0 TO 2**PWM_RES-1;
 	signal data : A_DATA;
 	signal o_pwm : std_logic_vector(PWM_RES-1 downto 0);
 	signal ld : std_logic_vector(LEDS-1 downto 0);
-	constant T_WAIT0 : integer := (BOARD_CLOCK/((2**PWM_RES)*2))-1; -- XXX sim
+	
+	constant S_WAIT0_1 : integer := 1;
+	constant S_WAIT0_2 : integer := 2;
+	constant S_WAIT0_3 : integer := 3;
+	constant S_WAIT0_4 : integer := 4;
+	constant S_WAIT0_5 : integer := 5;
+	constant S_WAIT0_6 : integer := 6;
+	constant S_WAIT0_7 : integer := 7;
+	constant S_WAIT0_8 : integer := 8;
+	constant S_WAIT0_9 : integer := 9;
+	constant S_WAIT0_10 : integer := 10;
+	constant S_WAIT0_MIN : integer := 1;
+	constant S_WAIT0_MAX : integer := 10;
+	constant S_WAIT0_DEFAULT : integer := (BOARD_CLOCK/((2**PWM_RES)*S_WAIT0_MIN))-1;
+	signal S_WAIT0_INDEX : integer := 1;
+	signal S_WAIT0 : integer;
 	
 	type A_NUM_GAMMA is array(0 to LEDS-1) of integer range 0 to NUMBER_GAMMA_CORRECTION_GREEN;
 	signal v_index : A_NUM_GAMMA;
-	signal v_wait0 : integer range 0 to T_WAIT0;
+	signal v_wait0 : integer range 0 to S_WAIT0_DEFAULT;
 	signal v_direction : std_logic_vector(LEDS-1 downto 0);
 
+	signal db_btn : std_logic_vector(BUTTONS-1 downto 0);
+
 begin
+
+	c0to4 : FOR i in 0 to BUTTONS-1 GENERATE
+	btn0to4 : debounce
+	GENERIC MAP (G_BOARD_CLOCK => BOARD_CLOCK)
+	PORT MAP (
+		i_clk => clk,
+		i_btn => btn(i),
+		o_db_btn => db_btn(i)
+	);
+	END GENERATE c0to4;
 
 	c0to7 : FOR i IN 0 to LEDS-1 GENERATE
 	pwm0to7 : PWM_NEW
 	GENERIC MAP (PWM_WIDTH => PWM_RES) -- 0 to 255
 	PORT MAP (
 		i_clock => clk,
-		i_reset => btn(0),
+		i_reset => db_btn(0),
 		i_load => ld(i),
 		i_data => data(i),
 		o_pwm => o_pwm(i)
 	);
 	END GENERATE c0to7;
 
-	p0 : process (clk,btn(0)) is
+	p_a : process (db_btn) is
 	begin
-		if (btn(0) = '1') then
+		if (db_btn(1)='1') then
+			if (S_WAIT0_INDEX < S_WAIT0_10) then
+				S_WAIT0_INDEX <= S_WAIT0_INDEX + 1;
+			else
+				S_WAIT0_INDEX <= S_WAIT0_10;
+			end if;
+		end if;
+		if (db_btn(2)='1') then
+			if (S_WAIT0_INDEX > S_WAIT0_1) then
+				S_WAIT0_INDEX <= S_WAIT0_INDEX - 1;
+			else
+				S_WAIT0_INDEX <= S_WAIT0_1;
+			end if;
+		end if;
+	end process p_a;
+
+	S_WAIT0 <= (BOARD_CLOCK/((2**PWM_RES)*S_WAIT0_1))-1 when S_WAIT0_INDEX=1 else
+	(BOARD_CLOCK/((2**PWM_RES)*S_WAIT0_2))-1 when S_WAIT0_INDEX=2 else
+	(BOARD_CLOCK/((2**PWM_RES)*S_WAIT0_3))-1 when S_WAIT0_INDEX=3 else
+	(BOARD_CLOCK/((2**PWM_RES)*S_WAIT0_4))-1 when S_WAIT0_INDEX=4 else
+	(BOARD_CLOCK/((2**PWM_RES)*S_WAIT0_5))-1 when S_WAIT0_INDEX=5 else
+	(BOARD_CLOCK/((2**PWM_RES)*S_WAIT0_6))-1 when S_WAIT0_INDEX=6 else
+	(BOARD_CLOCK/((2**PWM_RES)*S_WAIT0_7))-1 when S_WAIT0_INDEX=7 else
+	(BOARD_CLOCK/((2**PWM_RES)*S_WAIT0_8))-1 when S_WAIT0_INDEX=8 else
+	(BOARD_CLOCK/((2**PWM_RES)*S_WAIT0_9))-1 when S_WAIT0_INDEX=9 else
+	(BOARD_CLOCK/((2**PWM_RES)*S_WAIT0_10))-1 when S_WAIT0_INDEX=10 else
+	(BOARD_CLOCK/((2**PWM_RES)*S_WAIT0_1))-1;
+		
+	p0 : process (clk,db_btn(0)) is
+	begin
+		if (db_btn(0) = '1') then
 			state <= start;
 			v_direction <= (others => '0');
 			v_wait0 <= 0;
@@ -155,7 +223,7 @@ begin
 						data(7) <= to_integer(unsigned(C_GAMMA_CORRECTION_GREEN(v_index(7))));
 					end if;
 				when wait0 =>
-					if (v_wait0 < T_WAIT0) then
+					if (v_wait0 < S_WAIT0) then
 						state <= wait0;
 						v_wait0 <= v_wait0 + 1;
 						ld(0) <= '0';
