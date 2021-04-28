e5b3f03a2de3f3d44e03f656da7e32dc2a1dd6f3
kedziorno <kjoblak85@gmail.com>
2021-04-28 17:21:56 +0200

    ones_detector - work - tb patterns,assert
    ones_detector - work - test 21 ones patterns
    
    ones_detector - work - add 2 bad test patterns
    
    ones_detector - work - cleanup
    
    ones_detector - work - add assert condition tb

--- vhdl_primitive/ones_detector.vhd
+++ vhdl_primitive/ones_detector.vhd
@@ -45,99 +45,40 @@ architecture Behavioral of ones_detector is
 	port (A,B,Cin:in STD_LOGIC;S,Cout:out STD_LOGIC);
 	end component FULL_ADDER;
 
-signal s0,c0 : std_logic_vector(N-1 downto 0);
-signal s1,c1 : std_logic_vector(N-1 downto 0);
-signal s2,c2 : std_logic_vector(N-1 downto 0);
+	signal s0,c0 : std_logic_vector(N-1 downto 0);
+	signal s1,c1 : std_logic_vector(N-1 downto 0);
+	signal s2,c2 : std_logic_vector(N-1 downto 0);
+
 begin
 
 g0 : for i in 1 to (N/2)-1 generate
 	g00 : if (i=1) generate
-		fa : FULL_ADDER port map (
-		A=>x(2),
-		B=>x(3),
-		Cin=>x(1),
-		
-		S=>s0(1),
-		Cout=>c0(1));	
+		fa : FULL_ADDER port map (A=>x(2),B=>x(3),Cin=>x(1),S=>s0(1),Cout=>c0(1));
 	end generate g00;
 	g01 : if (i>1 and i<(N/2)-1) generate
-		fa : FULL_ADDER port map (
-		A=>x(2*i+0),
-		B=>x(2*i+1),
-		Cin=>s0(i-1),
-		
-		S=>s0(i),
-		Cout=>c0(i));
+		fa : FULL_ADDER port map (A=>x(2*i+0),B=>x(2*i+1),Cin=>s0(i-1),S=>s0(i),Cout=>c0(i));
 	end generate g01;
 	g02 : if (i=(N/2)-1) generate
-		fa : FULL_ADDER port map (
-		A=>x(2*i+0),
-		B=>x(2*i+1),
-		Cin=>s0(i-1),
-		
-		S=>y(0),
-		Cout=>c0(i));
+		fa : FULL_ADDER port map (A=>x(2*i+0),B=>x(2*i+1),Cin=>s0(i-1),S=>y(0),Cout=>c0(i));
 	end generate g02;
 end generate g0;
 
 g1 : for i in 1 to (N/2/2)-1 generate
 	g10 : if (i=1) generate
-		fa : FULL_ADDER port map (
-		A=>c0(2*i),
-		B=>c0(2*i+1),
-		Cin=>c0(2*i-1),
-		
-		S=>s1(1), --XXX
-		Cout=>c1(1)); --XXX
+		fa : FULL_ADDER port map (A=>c0(2*i),B=>c0(2*i+1),Cin=>c0(2*i-1),S=>s1(1),Cout=>c1(1));
 	end generate g10;
 	g11 : if (i>1 and i<(N/2/2)-1) generate
-		fa : FULL_ADDER port map (
-		A=>c0(2*i),
-		B=>c0(2*i+1),
-		Cin=>s1(i-1),
-		
-		S=>s1(i), --XXX
-		Cout=>c1(i)); --XXX
+		fa : FULL_ADDER port map (A=>c0(2*i),B=>c0(2*i+1),Cin=>s1(i-1),S=>s1(i),Cout=>c1(i));
 	end generate g11;
 	g12 : if (i=(N/2/2)-1) generate
-		fa : FULL_ADDER port map (
-		A=>c0(2*i),
-		B=>c0(2*i+1),
-		Cin=>s1(i-1),
-		
-		S=>y(1), --XXX
-		Cout=>c1(i)); --XXX
+		fa : FULL_ADDER port map (A=>c0(2*i),B=>c0(2*i+1),Cin=>s1(i-1),S=>y(1),Cout=>c1(i));
 	end generate g12;
 end generate g1;
 
---g2 : for i in 1 to (N/2/2/2)-1 generate
---	g20 : if (i=1) generate
---		fa : FULL_ADDER port map (
---		A=>c1(2*i),
---		B=>c1(2*i+1),
---		Cin=>c1(2*i-1),
---		
---		S=>s2(1), --XXX
---		Cout=>c2(1)); --XXX
---	end generate g20;
---	g21 : if (i>1 and i<(N/2/2/2)-1) generate
---		fa : FULL_ADDER port map (
---		A=>c1(2*i),
---		B=>c1(2*i+1),
---		Cin=>s1(2*i-1),
---		
---		S=>s2(i), --XXX
---		Cout=>c2(i)); --XXX
---	end generate g21;
---	g22 : if (i=(N/2/2/2)-1) generate
---		fa : FULL_ADDER port map (
---		A=>c1(2*i),
---		B=>c1(2*i+1),
---		Cin=>s1(2*i-1),
---		
---		S=>y(2), --XXX
---		Cout=>y(3)); --XXX
---	end generate g22;
---end generate g2;
+g2 : for i in 1 to (N/2/2/2)-1 generate
+	g22 : if (i=(N/2/2/2)-1) generate
+		fa : FULL_ADDER port map (A=>c1(2),B=>c1(3),Cin=>c1(1),S=>y(2),Cout=>y(3));
+	end generate g22;
+end generate g2;
 
 end Behavioral;
--- vhdl_primitive/tb_ones_detector.vhd
+++ vhdl_primitive/tb_ones_detector.vhd
@@ -30,7 +30,7 @@ USE ieee.std_logic_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---USE ieee.numeric_std.ALL;
+USE ieee.numeric_std.ALL;
 
 ENTITY tb_ones_detector IS
 END tb_ones_detector;
@@ -61,10 +61,57 @@ y => y
 
 -- Stimulus process
 stim_proc: process
+constant N : integer := 23;
+type array_data_expect is array(integer range <>) of std_logic_vector(3 downto 0);
+variable data_expect : array_data_expect(0 to N-1) := (
+x"0", -- XXX bad
+x"7",x"F",x"0",x"8",
+x"8",x"B",x"c",x"3",
+x"4",x"7",x"4",x"4",
+x"b",x"b",x"c",x"5",
+x"6",x"a",x"5",x"6",
+x"6",
+x"1" -- XXX bad
+);
+type array_data_in is array(integer range <>) of std_logic_vector(15 downto 1);
+variable data_in : array_data_in(0 to N-1) := (
+std_logic_vector(to_unsigned(to_integer(unsigned'(x"DEAD")),15)),
+"010101010101010",
+"111111111111111",
+"000000000000000",
+"101010101010101",
+
+"110011001100110",
+"110111011101110",
+"111011101110111",
+"000100010001000",
+
+"001000100010001",
+"001100110011001",
+"010001000100001",
+"100010001000100",
+
+"011101110111011",
+"101110111011101",
+"111011011101111",
+"100000010001110",
+
+"001010101000110",
+"100111001111011",
+"000010000111001",
+"100010000100111",
+
+"000011111100000",
+std_logic_vector(to_unsigned(to_integer(unsigned'(x"CAFE")),15))
+);
 begin		
 -- insert stimulus here
+loop0 : for i in 0 to N-1 loop
+x <= data_in(i);
+wait for 10 ns;
+assert (y=data_expect(i)) report "error at " & integer'image(i) & " : y is " & integer'image(to_integer(unsigned(y(3 downto 0)))) & " , expect " & integer'image(to_integer(unsigned(data_expect(i)))); 
 wait for 500 ns;
-x <= "010101010101010";
+end loop loop0;
 wait;
 end process;
 
