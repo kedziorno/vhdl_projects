4fbaf9627836abed72fddeb90f0fa731abf141df
kedziorno <kjoblak85@gmail.com>
2021-05-04 21:41:55 +0200

    logicanalyser - wip - simulate test1

--- vhdl_primitive/logic_analyser.vhd
+++ vhdl_primitive/logic_analyser.vhd
@@ -30,7 +30,18 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity logic_analyser is
-
+Generic (
+G_BOARD_CLOCK : integer := 50_000_000;
+G_BAUD_RATE : integer := 9_600;
+address_size : integer := 8;
+data_size : integer := 8
+);
+Port (
+i_clock : in std_logic;
+i_reset : in std_logic;
+i_data : in std_logic_vector(data_size-1 downto 0);
+o_rs232_tx : out std_logic
+);
 end logic_analyser;
 
 architecture Behavioral of logic_analyser is
@@ -74,6 +85,110 @@ o_q : inout std_logic_vector(N-1 downto 0)
 );
 end component ripple_counter;
 
+component rs232 is
+Generic (
+G_BOARD_CLOCK : integer := G_BOARD_CLOCK;
+G_BAUD_RATE : integer := G_BAUD_RATE
+);
+Port(
+clk : in  STD_LOGIC;
+rst : in  STD_LOGIC;
+enable_tx : in  STD_LOGIC;
+enable_rx : in  STD_LOGIC;
+byte_to_send : in  STD_LOGIC_VECTOR (8 downto 0);
+byte_received : out  STD_LOGIC_VECTOR (7 downto 0);
+parity_tx : out  STD_LOGIC;
+parity_rx : out  STD_LOGIC;
+busy : out  STD_LOGIC;
+ready : out  STD_LOGIC;
+is_byte_received : out STD_LOGIC;
+RsTx : out  STD_LOGIC;
+RsRx : in  STD_LOGIC
+);
+end component rs232;
+
+signal latch_le,latch_oeb : std_logic;
+signal latch_d,latch_q : std_logic_vector(data_size-1 downto 0);
+signal sram_ceb,sram_web,sram_oeb : std_logic;
+signal sram_address : std_logic_vector(address_size-1 downto 0);
+signal sram_di,sram_do : std_logic_vector(data_size-1 downto 0);
+signal rc_clock,rc_cpb,rc_mrb : std_logic;
+signal rc_oq : std_logic_vector(address_size-1 downto 0);
+signal rs232_clock,rs232_reset,rs232_etx,rs232_tx,rs232_rx,rs232_busy : std_logic;
+signal rs232_b2s : std_logic_vector(8 downto 0);
+
+signal oc0,ain1,wr,rd,a,b : std_logic;
+
 begin
+
+oc0 <= i_clock;
+ain1 <= i_clock;
+rs232_etx <= i_clock;
+wr <= rc_oq(address_size-1);
+a <= oc0 and wr;
+sram_web <= not a;
+latch_le <= a;
+sram_oeb <= not ain1;
+b <= a and ain1;
+sram_ceb <= '0';
+rc_clock <= b;
+rc_cpb <= '1';
+rc_mrb <= rd;
+rs232_reset <= i_reset;
+latch_oeb <= oc0;
+rd <= '1' when falling_edge(rs232_busy) else '0';
+
+latch_d <= i_data;
+sram_di <= latch_q;
+sram_address <= rc_oq;
+rs232_b2s(7 downto 0) <= sram_do;
+o_rs232_tx <= rs232_tx;
+
+latch_entity : nxp_74hc573
+generic map (nbit=>data_size)
+port map (
+i_le=>latch_le,
+i_oeb=>latch_oeb,
+i_d=>latch_d,
+o_q=>latch_q
+);
+
+sram_entity : sram_62256
+Generic map (address_size=>address_size,data_size=>data_size)
+Port map (
+i_ceb=>sram_ceb,
+i_web=>sram_web,
+i_oeb=>sram_oeb,
+i_address=>sram_address,
+i_data=>sram_di,
+o_data=>sram_do
+);
+
+rc_entity : ripple_counter
+Generic map (N=>address_size)
+Port map (
+i_clock=>rc_clock,
+i_cpb=>rc_cpb,
+i_mrb=>rc_mrb,
+o_q=>rc_oq
+);
+
+rs232_entity : rs232
+Generic map (G_BOARD_CLOCK=>50_000_000,G_BAUD_RATE=>9_600)
+Port map (
+clk=>rs232_clock,
+rst=>rs232_reset,
+enable_tx=>rs232_etx,
+enable_rx=>'0',
+byte_to_send=>rs232_b2s,
+byte_received=>open,
+parity_tx=>open,
+parity_rx=>open,
+busy=>rs232_busy,
+ready=>open,
+is_byte_received=>open,
+RsTx=>rs232_tx,
+RsRx=>rs232_rx
+);
 
 end Behavioral;
--- vhdl_primitive/rs232.vhd
+++ vhdl_primitive/rs232.vhd
@@ -29,83 +29,277 @@ use IEEE.NUMERIC_STD.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity rs232_1 is
-Port(
-	clk : in  STD_LOGIC_VECTOR (0 downto 0);
-	rst : in  STD_LOGIC_VECTOR (0 downto 0);
-	RsTx : out  STD_LOGIC_VECTOR (0 downto 0);
-	RsRx : in  STD_LOGIC_VECTOR (0 downto 0)
+entity rs232 is
+Generic (
+	G_BOARD_CLOCK : integer := 50_000_000;
+	G_BAUD_RATE : integer := 9_600
 );
-end rs232_1;
+Port(
+	clk : in  STD_LOGIC;
+	rst : in  STD_LOGIC;
+	enable_tx : in  STD_LOGIC;
+	enable_rx : in  STD_LOGIC;
+	byte_to_send : in  STD_LOGIC_VECTOR (8 downto 0);
+	byte_received : out  STD_LOGIC_VECTOR (7 downto 0);
+	parity_tx : out  STD_LOGIC;
+	parity_rx : out  STD_LOGIC;
+	busy : out  STD_LOGIC;
+	ready : out  STD_LOGIC;
+	is_byte_received : out STD_LOGIC;
+	RsTx : out  STD_LOGIC;
+	RsRx : in  STD_LOGIC
+);
+end rs232;
 
-architecture Behavioral of rs232_1 is
-	constant CLK_BOARD : integer := 50_000_000;
-	constant BAUD_RATE : integer := 9_600;
+architecture Behavioral of rs232 is
 
-	signal clk_div1 : std_logic_vector (0 downto 0);
-	
-	constant NUMBER_BITS : integer := 8;
-	signal send_byte_index : std_logic_vector(NUMBER_BITS-1 downto 0) := x"00";
+	constant recv_bits : integer := 10;
+	constant a : integer := (G_BOARD_CLOCK/G_BAUD_RATE);
 
---	signal byte_to_send : std_logic_vector (NUMBER_BITS-1 downto 0) := "01000001";
-	signal byte_to_send : std_logic_vector (NUMBER_BITS-1 downto 0) := "10101010";
---	signal byte_to_send : std_logic_vector (NUMBER_BITS-1 downto 0) := "01010101";
---	signal byte_to_send : std_logic_vector (NUMBER_BITS-1 downto 0) := "11111111";
---	signal byte_to_send : std_logic_vector (NUMBER_BITS-1 downto 0) := "00000000";
-	
-	type state is (start,increment_index,send_byte,send_byte_last,stop);
-	signal c_state,n_state : state := start;
+	signal v_i : std_logic_vector(31 downto 0);
+	signal v_w : std_logic_vector(31 downto 0);
+	signal t_w : std_logic_vector(31 downto 0);
+	signal temp : std_logic_vector(recv_bits - 1 downto 0);
+	signal p_tx,p_rx : std_logic;
+
+	type t_state is (
+		idle,
+		start,wstart,
+		b1,wb1,b2,wb2,b3,wb3,b4,wb4,b5,wb5,b6,wb6,b7,wb7,b8,wb8,
+		parity,wparity,
+		stop,wstop
+	);
+	signal tx_state : t_state;
+
+	type r_state is (
+		idle,
+		start,
+		recv,
+		wait0,
+		increment,
+		parity,
+		wparity,
+		stop
+	);
+	signal rx_state : r_state;
+
 begin
 
-	p_dv : process (clk(0),c_state,rst) is
-		variable COUNTER_BAUD_RATE_MAX : integer := (CLK_BOARD/BAUD_RATE);
-		variable counter_baud_rate : integer := 0;
+	p0 : process (clk,rst) is -- rx mode
 	begin
-		if (rst(0) = '1') then
-			send_byte_index <= x"00";
-		elsif (rising_edge(clk(0))) then
-			if (counter_baud_rate < COUNTER_BAUD_RATE_MAX-1) then
-				clk_div1 <= std_logic_vector(to_unsigned(0,1));
-				counter_baud_rate := counter_baud_rate + 1;
-			else
-				clk_div1 <= std_logic_vector(to_unsigned(1,1));
-				counter_baud_rate := 0;
-				if (c_state = send_byte) then
-					if (to_integer(unsigned(send_byte_index)) < NUMBER_BITS-1) then
-						send_byte_index <= std_logic_vector(unsigned(send_byte_index)+1);
-					else
-						send_byte_index <= x"00";
+		if (rst = '1') then
+			rx_state <= idle;
+			v_i <= (others => '0');
+			v_w <= (others => '0');
+			temp <= (others => '0');
+		elsif (rising_edge(clk)) then
+			case (rx_state) is
+				when idle =>
+					if (enable_rx = '1') then
+						rx_state <= start;
+						v_i <= (others => '0');
+						v_w <= (others => '0');
+						is_byte_received <= '0';
+					elsif (enable_rx = '0') then
+						rx_state <= idle;
 					end if;
-				end if;
-			end if;
+				when start =>
+					if (RsRx = '1') then
+						if (to_integer(unsigned(v_i)) = a-1) then
+							rx_state <= recv;
+							v_i <= x"00000001"; -- we receive first bit
+							temp(0) <= RsRx;
+						else
+							rx_state <= start;
+							v_i <= std_logic_vector(to_unsigned(to_integer(unsigned(v_i)) + 1,32));
+						end if;
+					elsif (RsRx = '0') then
+						rx_state <= start;
+						v_i <= (others => '0');
+					end if;
+				when recv =>
+					rx_state <= wait0;
+					temp(to_integer(unsigned(v_i))) <= RsRx;
+				when wait0 =>
+					if (to_integer(unsigned(v_w)) = a-1) then
+						rx_state <= increment;
+						v_w <= (others => '0');
+					else
+						rx_state <= wait0;
+						v_w <= std_logic_vector(to_unsigned(to_integer(unsigned(v_w)) + 1,32));
+					end if;
+				when increment =>
+					if (to_integer(unsigned(v_i)) = recv_bits-2) then
+						rx_state <= parity;
+						v_i <= (others => '0');
+					else
+						rx_state <= recv;
+						v_i <= std_logic_vector(to_unsigned(to_integer(unsigned(v_i)) + 1,32));
+					end if;
+				when parity =>
+					rx_state <= wparity;
+					p_rx <= temp(1) xor temp(2) xor temp(3) xor temp(4) xor temp(5) xor temp(6) xor temp(7) xor temp(8);
+				when wparity =>
+					if (to_integer(unsigned(v_w)) = a-1) then
+						rx_state <= stop;
+						v_w <= (others => '0');
+					else
+						rx_state <= wparity;
+						v_w <= std_logic_vector(to_unsigned(to_integer(unsigned(v_w)) + 1,32));
+					end if;
+				when stop =>
+					rx_state <= idle;
+					is_byte_received <= '1';
+					byte_received <= temp(recv_bits-2 downto 1);
+					parity_rx <= p_rx; -- recv_bits-1
+			end case;
 		end if;
-	end process p_dv;
-
-	p0 : process (clk_div1(0),c_state) is
-	begin
-		if (rising_edge(clk_div1(0))) then
-			c_state <= n_state;
-		end if;
-		case c_state is
-			when start =>
-				RsTx <= std_logic_vector(to_unsigned(0,1));
-				n_state <= send_byte;				
-			when send_byte =>				
-				if (to_integer(unsigned(send_byte_index)) < NUMBER_BITS-1) then
-					n_state <= send_byte;
-				else
-					n_state <= stop;
-				end if;
-				if (byte_to_send(to_integer(NUMBER_BITS-1-unsigned(send_byte_index))) = '0') then
-					RsTx <= std_logic_vector(to_unsigned(0,1));
-				elsif (byte_to_send(to_integer(NUMBER_BITS-1-unsigned(send_byte_index))) = '1') then
-					RsTx <= std_logic_vector(to_unsigned(1,1));
-				end if;
-			when stop =>
-				RsTx <= std_logic_vector(to_unsigned(1,1));
-				n_state <= start;
-			when others => null;
-		end case;
 	end process p0;
+
+	p1 : process (clk,rst) is -- tx mode
+	begin
+		if (rst = '1') then
+			tx_state <= start;
+			busy <= '0';
+			ready <= '1';
+			RsTx <= '0';
+			t_w <= (others => '0');
+		elsif (rising_edge(clk)) then
+			case tx_state is
+				when idle =>
+					if (enable_tx = '1') then
+						tx_state <= start;
+					end if;
+				when start =>
+					tx_state <= wstart;
+					busy <= '1';
+					ready <= '0';
+					RsTx <= '1';
+				when wstart =>
+					if (to_integer(unsigned(t_w)) = a-1) then
+						tx_state <= b1;
+						t_w <= (others => '0');
+					else
+						tx_state <= wstart;
+						t_w <= std_logic_vector(to_unsigned(to_integer(unsigned(t_w)) + 1,32));
+					end if;
+				when b1 =>
+					tx_state <= wb1;
+					RsTx <= byte_to_send(0);
+				when wb1 =>
+					if (to_integer(unsigned(t_w)) = a-1) then
+						tx_state <= b2;
+						t_w <= (others => '0');
+					else
+						tx_state <= wb1;
+						t_w <= std_logic_vector(to_unsigned(to_integer(unsigned(t_w)) + 1,32));
+					end if;
+				when b2 =>
+					tx_state <= wb2;
+					RsTx <= byte_to_send(1);
+				when wb2 =>
+					if (to_integer(unsigned(t_w)) = a-1) then
+						tx_state <= b3;
+						t_w <= (others => '0');
+					else
+						tx_state <= wb2;
+						t_w <= std_logic_vector(to_unsigned(to_integer(unsigned(t_w)) + 1,32));
+					end if;
+				when b3 =>
+					tx_state <= wb3;
+					RsTx <= byte_to_send(2);
+				when wb3 =>
+					if (to_integer(unsigned(t_w)) = a-1) then
+						tx_state <= b4;
+						t_w <= (others => '0');
+					else
+						tx_state <= wb3;
+						t_w <= std_logic_vector(to_unsigned(to_integer(unsigned(t_w)) + 1,32));
+					end if;
+				when b4 =>
+					tx_state <= wb4;
+					RsTx <= byte_to_send(3);
+				when wb4 =>
+					if (to_integer(unsigned(t_w)) = a-1) then
+						tx_state <= b5;
+						t_w <= (others => '0');
+					else
+						tx_state <= wb4;
+						t_w <= std_logic_vector(to_unsigned(to_integer(unsigned(t_w)) + 1,32));
+					end if;
+				when b5 =>
+					tx_state <= wb5;
+					RsTx <= byte_to_send(4);
+				when wb5 =>
+					if (to_integer(unsigned(t_w)) = a-1) then
+						tx_state <= b6;
+						t_w <= (others => '0');
+					else
+						tx_state <= wb5;
+						t_w <= std_logic_vector(to_unsigned(to_integer(unsigned(t_w)) + 1,32));
+					end if;
+				when b6 =>
+					tx_state <= wb6;
+					RsTx <= byte_to_send(5);
+				when wb6 =>
+					if (to_integer(unsigned(t_w)) = a-1) then
+						tx_state <= b7;
+						t_w <= (others => '0');
+					else
+						tx_state <= wb6;
+						t_w <= std_logic_vector(to_unsigned(to_integer(unsigned(t_w)) + 1,32));
+					end if;
+				when b7 =>
+					tx_state <= wb7;
+					RsTx <= byte_to_send(6);
+				when wb7 =>
+					if (to_integer(unsigned(t_w)) = a-1) then
+						tx_state <= b8;
+						t_w <= (others => '0');
+					else
+						tx_state <= wb7;
+						t_w <= std_logic_vector(to_unsigned(to_integer(unsigned(t_w)) + 1,32));
+					end if;
+				when b8 =>
+					tx_state <= wb8;
+					RsTx <= byte_to_send(7);
+				when wb8 =>
+					if (to_integer(unsigned(t_w)) = a-1) then
+						tx_state <= parity;
+						t_w <= (others => '0');
+					else
+						tx_state <= wb8;
+						t_w <= std_logic_vector(to_unsigned(to_integer(unsigned(t_w)) + 1,32));
+					end if;
+				when parity =>
+					tx_state <= wparity;
+					p_tx <= byte_to_send(0) xor byte_to_send(1) xor byte_to_send(2) xor byte_to_send(3) xor byte_to_send(4) xor byte_to_send(5) xor byte_to_send(6) xor byte_to_send(7);
+					RsTx <= p_tx;
+				when wparity =>
+					if (to_integer(unsigned(t_w)) = a-1) then
+						tx_state <= stop;
+						t_w <= (others => '0');
+					else
+						tx_state <= wparity;
+						t_w <= std_logic_vector(to_unsigned(to_integer(unsigned(t_w)) + 1,32));
+					end if;
+				when stop =>
+					tx_state <= wstop;
+					RsTx <= '0';
+					busy <= '0';
+					ready <= '1';
+				when wstop =>
+					if (to_integer(unsigned(t_w)) = a-1) then
+						tx_state <= idle;
+						parity_tx <= p_tx;
+						t_w <= (others => '0');
+					else
+						tx_state <= wstop;
+						t_w <= std_logic_vector(to_unsigned(to_integer(unsigned(t_w)) + 1,32));
+					end if;
+				when others => null;
+			end case;
+		end if;
+	end process p1;
 
 end Behavioral;
