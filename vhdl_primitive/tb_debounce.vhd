a613c61b3985bcac7488ab2a28795b24d22d480c
kedziorno <kjoblak85@gmail.com>
2021-07-05 18:56:39 +0200

    vhdl_primitive - logic_analyser - wip - modify tb debounce

--- vhdl_primitive/tb_debounce.vhd
+++ vhdl_primitive/tb_debounce.vhd
@@ -27,6 +27,7 @@
 --------------------------------------------------------------------------------
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
+USE work.p_globals.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -38,21 +39,62 @@ END tb_debounce;
 ARCHITECTURE behavior OF tb_debounce IS
 
 	-- Constant
-	constant SIZE : integer := 8;
-	constant G_BOARD_CLOCK : integer := 50_000_000;
+	constant DEBOUNCE_SIZE : integer := 8;
+	constant W0_COUNT : integer := 80;
+	constant G_BOARD_CLOCK : integer := 29_952_000;
+	constant LFSR_SIZE : integer := 32;
+	constant LFSR_SIZE_BITS : integer := 32;
+	constant GRAYCODE_SIZE : integer := 8;
+	constant GRAYCODE_SIZE_BITS : integer := 8;
 
 	-- Component Declaration for the Unit Under Test (UUT)
-	COMPONENT debouncer1 IS
-	GENERIC (
-		fclk : integer := 1;
-		twindow : integer := 10
+--	COMPONENT debouncer1 IS
+--	GENERIC (
+--		fclk : integer := 1;
+--		twindow : integer := 10
+--	);
+--	PORT (
+--		x : in std_logic;
+--		clk : in std_logic;
+--		y : inout std_logic
+--	);
+--	END COMPONENT debouncer1;
+
+--	component debounce_button is
+--	generic (g_board_clock : integer);
+--	Port
+--	(
+--	i_button : in  STD_LOGIC;
+--	i_clk : in  STD_LOGIC;
+--	o_stable : out  STD_LOGIC
+--	);
+--	end component debounce_button;
+
+--	component debounce is
+--	Generic (
+--		G_BOARD_CLOCK : integer := 50_000_000;
+--		G_SIZE : integer := 8
+--	);
+--	Port (
+--		i_clk : in  STD_LOGIC;
+--		i_reset : in  STD_LOGIC;
+--		i_btn : in  STD_LOGIC;
+--		o_db_btn : out  STD_LOGIC
+--	);
+--	end component debounce;
+
+	component new_debounce is
+	generic (
+	G_BOARD_CLOCK : integer := 1;
+	stable : integer := 1
 	);
-	PORT (
-		x : in std_logic;
-		clk : in std_logic;
-		y : inout std_logic
+	port (
+	i_clock : in std_logic;
+	i_reset : in std_logic;
+	i_b : in std_logic;
+	o_db : out std_logic
 	);
-	END COMPONENT debouncer1;
+	end component new_debounce;
 
 	COMPONENT lfsr1 IS
 	GENERIC (G_SIZE : integer);
@@ -79,39 +121,74 @@ ARCHITECTURE behavior OF tb_debounce IS
 	signal i_clk : std_logic := '0';
 	signal i_btn : std_logic := '0';
 	signal reset : std_logic := '0';
+	signal reset_db : std_logic := '0';
 	signal enable_gc : std_logic := '0';
 	signal enable_lfsr : std_logic := '0';
-	signal i_int : std_logic_vector (SIZE-1 downto 0) := (others => '0');
+	signal i_int : std_logic_vector (GRAYCODE_SIZE_BITS-1 downto 0) := (others => '0');
 
 	--Outputs
 	signal o_db_btn : std_logic;
-	signal o_gc : std_logic_vector (SIZE-1 downto 0);
-	signal o_lfsr : std_logic_vector (SIZE-1 downto 0);
+	signal o_gc : std_logic_vector (GRAYCODE_SIZE_BITS-1 downto 0);
+	signal o_lfsr : std_logic_vector (LFSR_SIZE_BITS-1 downto 0);
 
 	-- Clock period definitions
 	constant i_clk_period : time := (1_000_000_000/G_BOARD_CLOCK) * 1 ns; -- XXX 50Mhz
 
 	-- States
-	type state_type is (idle,start,lfsr_enable,lfsr_disable,lfsr_send,lfsr_increment,lfsr_wait0,gc_send,gc_increment,gc_wait0,stop);
+	type state_type is (idle,start,
+	lfsr_enable,lfsr_disable,lfsr_send,lfsr_increment,lfsr_wait0,
+	gc_send,gc_increment,gc_wait0,
+	stop);
 	signal state : state_type := idle;
 	signal simulation_finish : std_logic := '0';
 
 BEGIN
 
 	-- Instantiate the Unit Under Test (UUT)
-	uut : debouncer1
-	GENERIC MAP (
-		fclk => 1,
-		twindow => SIZE
+--	uut : debouncer1
+--	GENERIC MAP (
+--		fclk => 1,
+--		twindow => SIZE
+--	)
+--	PORT MAP (
+--		x => i_btn,
+--		clk => i_clk,
+--		y => o_db_btn
+--	);
+
+--	uut : debounce_button
+--	generic map (g_board_clock => G_BOARD_CLOCK)
+--	port map (
+--	i_button => i_btn,
+--	i_clk => i_clk,
+--	o_stable => o_db_btn
+--	);
+
+--	uut : debounce
+--	Generic map (
+--		G_BOARD_CLOCK => G_BOARD_CLOCK,
+--		G_SIZE => DEBOUNCE_SIZE
+--	)
+--	Port map (
+--		i_clk => i_clk,
+--		i_reset => reset,
+--		i_btn => i_btn,
+--		o_db_btn => o_db_btn
+--	);
+
+	uut : new_debounce
+	generic map(
+	G_BOARD_CLOCK => G_BOARD_CLOCK
 	)
-	PORT MAP (
-		x => i_btn,
-		clk => i_clk,
-		y => o_db_btn
+	port map (
+	i_clock => i_clk,
+	i_reset => reset_db,
+	i_b => i_btn,
+	o_db => o_db_btn
 	);
 
 	uut_lfsr: lfsr1
-	GENERIC MAP (G_SIZE => SIZE)
+	GENERIC MAP (G_SIZE => LFSR_SIZE_BITS)
 	PORT MAP (
 		reset => reset,
 		clk => i_clk,
@@ -120,7 +197,7 @@ BEGIN
 	);
 
 	uut_graycode: graycode
-	GENERIC MAP (G_SIZE => SIZE)
+	GENERIC MAP (G_SIZE => GRAYCODE_SIZE_BITS)
 	PORT MAP (
 		reset => reset,
 		clk => i_clk,
@@ -144,17 +221,17 @@ BEGIN
 	-- Stimulus process
 	stim_proc: process (i_clk) is
 
-		constant WAIT0_COUNT : integer := SIZE;
+		constant WAIT0_COUNT : integer := W0_COUNT;
 		variable wait0 : integer range 0 to WAIT0_COUNT-1 := 0;
 -- LFSR
-		variable index : integer range 0 to SIZE-1 := 0;
+		variable index : integer range 0 to LFSR_SIZE-1 := 0;
 		constant send_the_same : integer := 1;
 		variable send_the_same_index : integer range 0 to send_the_same-1 := 0;
 -- GRAYCODE
-		constant o_gc_max : integer := SIZE;
+		constant o_gc_max : integer := GRAYCODE_SIZE;
 		variable o_gc_index : integer range 0 to o_gc_max-1 := 0;
-		constant gc_max : std_logic_vector(SIZE-1 downto 0) := (others => '1');
-		variable gc_index : std_logic_vector(SIZE-1 downto 0) := (others => '0');
+		constant gc_max : std_logic_vector(GRAYCODE_SIZE_BITS-1 downto 0) := (others => '1');
+		variable gc_index : std_logic_vector(GRAYCODE_SIZE_BITS-1 downto 0) := (others => '0');
 
 	begin
 		-- insert stimulus here
@@ -168,6 +245,7 @@ BEGIN
 					state <= gc_send;
 					REPORT "GRAYCODE" SEVERITY NOTE;
 				when gc_send => -- start from gc mode
+					reset_db <= '0';
 					if (o_gc_index = o_gc_max-1) then
 						state <= gc_increment;
 						o_gc_index := 0;
@@ -182,10 +260,10 @@ BEGIN
 					if (to_integer(unsigned(gc_index)) = to_integer(unsigned(gc_max))-1) then
 						state <= lfsr_enable; -- jump to lfsr mode
 						REPORT "LFSR" SEVERITY NOTE;
-						gc_index := std_logic_vector(to_unsigned(0,SIZE));
+						gc_index := std_logic_vector(to_unsigned(0,GRAYCODE_SIZE_BITS));
 					else
 						state <= gc_wait0;
-						gc_index := std_logic_vector(to_unsigned(to_integer(unsigned(gc_index) + 1),SIZE));
+						gc_index := std_logic_vector(to_unsigned(to_integer(unsigned(gc_index) + 1),GRAYCODE_SIZE_BITS));
 						i_int <= gc_index;
 					end if;
 				when gc_wait0 =>
@@ -196,15 +274,17 @@ BEGIN
 					else
 						state <= gc_send;
 						wait0 := 0;
+						reset_db <= '1';
 					end if;
 				when lfsr_enable =>
 					state <= lfsr_disable;
 					enable_lfsr <= '1';
+					reset_db <= '0';
 				when lfsr_disable =>
 					state <= lfsr_send;
 					enable_lfsr <= '0';
 				when lfsr_send =>
-					if (index = SIZE-1) then
+					if (index = LFSR_SIZE-1) then
 						state <= lfsr_increment;
 						index := 0;
 					else
@@ -213,7 +293,7 @@ BEGIN
 						index := index + 1;
 					end if;
 				when lfsr_increment =>
-					if (o_lfsr = std_logic_vector(to_unsigned(0,SIZE))) then
+					if (o_lfsr = std_logic_vector(to_unsigned(0,LFSR_SIZE_BITS))) then
 						state <= stop;
 					else
 						state <= lfsr_wait0;
@@ -222,6 +302,7 @@ BEGIN
 					if (wait0 = WAIT0_COUNT-1) then
 						state <= lfsr_enable;
 						wait0 := 0;
+						reset_db <= '1';
 					else
 						state <= lfsr_wait0;
 						wait0 := wait0 + 1;
