da92e4d7e39fe6565f4ec02bd46f431d2fe7fc74
kedziorno <kjoblak85@gmail.com>
2021-07-05 18:53:31 +0200

    vhdl_primitive - logic_analyser - wip - add reset to ff_d_pe

--- vhdl_primitive/FF_D_PE.vhd
+++ vhdl_primitive/FF_D_PE.vhd
@@ -2,7 +2,11 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity FF_D_POSITIVE_EDGE is
-port (C,D:in STD_LOGIC;Q1,Q2:inout STD_LOGIC);
+port (
+i_reset : in std_logic := '0';
+C : in std_logic;
+D : in STD_LOGIC;
+Q1,Q2:inout STD_LOGIC);
 end entity FF_D_POSITIVE_EDGE;
 
 -- https://en.wikipedia.org/wiki/Flip-flop_(electronics)#Classical_positive-edge-triggered_D_flip-flop
@@ -20,18 +24,30 @@ for all : GN use entity WORK.GATE_NOT(GATE_NOT_BEHAVIORAL_1);
 signal X,Y,Z,V,W,O,sa,sb,sc,sd,se,sf:STD_LOGIC;
 constant DELAY_AND : time := 1 ps;
 constant DELAY_NOT : time := 1 ps;
+signal rst1,rst2,rst3 : std_logic;
 begin
-g1: GAND generic map (DELAY_AND) port map (D,X,sa);
+
+rst1 <= D when i_reset = '0' else '0';
+g1: GAND generic map (DELAY_AND) port map (rst1,X,sa);
 g2: GN generic map (DELAY_NOT) port map(sa,Y);
+
 g3: GAND generic map (DELAY_AND) port map (Y,O,sb);
 g4: GN generic map (DELAY_NOT) port map(sb,X);
-g5: GAND generic map (DELAY_AND) port map (C,V,sc);
+
+rst2 <= C when i_reset = '0' else '0';
+g5: GAND generic map (DELAY_AND) port map (rst2,V,sc);
 g6: GN generic map (DELAY_NOT) port map(sc,Z);
+
 g7: GAND generic map (DELAY_AND) port map (Z,Y,sd);
 g8: GN generic map (DELAY_NOT) port map(sd,V);
-g9: GAND generic map (DELAY_AND) port map (Z,Q2,se);
+
+rst3 <= Q2 when i_reset = '0' else '0';
+g9: GAND generic map (DELAY_AND) port map (Z,rst3,se);
 gA: GN generic map (DELAY_NOT) port map(se,Q1);
+
 gB: GAND generic map (DELAY_AND) port map (X,Q1,sf);
 gC: GN generic map (DELAY_NOT) port map(sf,Q2);
+
 gD: GAND generic map (DELAY_AND) port map (C,Z,O);
+
 end architecture Behavioral_D_PE;
