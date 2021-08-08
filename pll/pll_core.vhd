74e38b437efd3e2c80108eebd30cda507cf8b121
kedziorno <kjoblak85@gmail.com>
2021-08-08 19:21:03 +0200

    pll - wip - add tb

--- pll/pll_core.vhd
+++ pll/pll_core.vhd
@@ -1,3 +1,7 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+
 entity pll_core is
 port (
 pll_arst_l : in bit;
@@ -13,38 +17,67 @@ signal neg_delay,ref_per,vco_per,vco_hi_ph,vco_lo_ph : time;
 signal t0,t1,posedge_vco_tmp,posedge_fdbkclk,insdelay,adj_delay : time := 0 ps;
 signal j,ph_offset_past,ph_offset : integer;
 signal pulse_cnt : integer := 2;
-signal mult,mult2x : bit_vector(6 downto 0);
+signal mult,mult2x : unsigned(6 downto 0) := (others => '0');
 signal vco_shift : bit;
 signal tmp_clk1,tmp_clk2,tmp_clk3,tmp_clk4 : bit;
 signal lock_cnt : bit_vector(2 downto 0);
 signal qnt_err : time;
-begin
 --synthesis translate_off
-mult <= div + '1';
-mult2x <= (mult(5 downto 0),'0'); 
+--https://groups.google.com/g/comp.lang.vhdl/c/OxgjdpZDOxU/m/SOB3tlzxQwgJ
+---------------------------------------------------------- to_sl ---
+---- convert 'bit' to 'std_logic' ---
+---------------------------------------------------------- to_sl ---
+function to_sl(b: bit) return std_logic is
+begin
+if b='1' then
+return '1';
+else
+return '0';
+end if;
+end;
+--------------------------------------------------------- to_slv ---
+---- convert 'bit_vector' to 'std_logic_vector' ---
+--------------------------------------------------------- to_slv ---
+function to_slv(bv:bit_vector) return std_logic_vector is
+variable sv: std_logic_vector(bv'RANGE);
+begin
+for i in bv'RANGE loop
+sv(i) := to_sl(bv(i));
+end loop;
+return sv;
+end;
+
+begin
+mult <= to_unsigned(to_integer(unsigned(to_slv(div))),7) + "1";
+mult2x <= (mult(5 downto 0)&"0"); 
 p0 : process (sysclk) is
 begin
-	if (rising_edge(sysclk)) then
 	t0 <= NOW;
+	if (sysclk'event and sysclk = '1') then
 	t1 <= NOW;
-	ref_per = t1 - t0;
-	vco_per = ref_per / (div+'1');
-	vco_hi_ph = ref_per / (mult2x);
-	vco_lo_ph = vco_per - vco_hi_ph; 
-	qnt_err = ref_per - (vco_hi_ph + vco_lo_ph) * mult;
+	ref_per <= t1 - t0;
+	vco_per <= time(ref_per / (to_integer(unsigned(to_slv(div)))+1));
+	vco_hi_ph <= ref_per / (to_integer(unsigned(mult2x)));
+	vco_lo_ph <= vco_per - vco_hi_ph; 
+	qnt_err <= ref_per - (vco_hi_ph + vco_lo_ph) * to_integer(unsigned(mult));
 	end if;
 end process p0;
 p1 : process (sysclk) is
+variable jj : integer := 0;
 begin
 	vco_tmp <= '1';
-	l0 : for j in 1 to mult loop
-		vco_tmp <= '0' after vco_hi_ph ns;
-		if ((j = shift_rigth(mult,1)) and (qnt_err /= 0)) then
-			j := j / 2;
+	l0 : for j in 1 to to_integer(mult) loop
+		jj := j;
+		vco_tmp <= '0' after vco_hi_ph;
+		if ((jj = shift_right(mult,1)) and (qnt_err /= 0 ns)) then
+			jj := jj / 2;
+		end if;
+		if (jj = 1) then
+			exit;
 		end if;
 		vco_tmp <= '1';
 	end loop l0;
-	vco_tmp <= '0'; after vco_hi_ph;
+	vco_tmp <= '0' after vco_hi_ph;
 end process p1;
 --always @ ( posedge  sysclk ) begin
 --	vco_tmp = 1'b1;
@@ -59,26 +92,27 @@ end process p1;
 p2 : process (vco_tmp,pll_arst_l) is
 begin
 	if (pll_arst_l = '0') then
-		posedge_vco_tmp <= 0;
-		posedge_fdbkclk <= 0;
-		adj_delay <= 0;
-		insdelay <= 0;
+		posedge_vco_tmp <= 0 ps;
+		posedge_fdbkclk <= 0 ps;
+		adj_delay <= 0 ps;
+		insdelay <= 0 ps;
 		ph_offset_past <= 360;		
-	elsif (rising_edge(vco_tmp)) then
-		if (locked) then
-			indelay <= insdelay;
+	elsif (vco_tmp'event and vco_tmp = '1') then
+		if (locked = '1') then
+			insdelay <= insdelay;
 		else
-			posedge_vco_tmp = NOW;
-			wait for rising_edge(fdbkclk);
-			posedge_fdbkclk = NOW;
-			insdelay = posedge_fdbkclk - posedge_vco_tmp;
+			posedge_vco_tmp <= NOW;
+--			wait until (fdbkclk'event and fdbkclk = '1');
+			posedge_fdbkclk <= NOW;
+			insdelay <= posedge_fdbkclk - posedge_vco_tmp;
 		end if;
 		l1 : while (vco_per <= insdelay) loop 
-			insdelay = insdelay - vco_per;
+			insdelay <= insdelay - vco_per;
 		end loop l1; 
-		adj_delay = vco_per - insdelay;
+		adj_delay <= vco_per - insdelay;
 	end if;
 end process p2;
+--XXX finish
 --always @ ( posedge vco_tmp or negedge pll_arst_l ) begin
 --	if (!pll_arst_l) begin
 --		posedge_vco_tmp = 0;
