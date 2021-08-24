267c3dea6bf5978cbe92c207b7e2c7aced120c79
kedziorno <kjoblak85@gmail.com>
2021-08-24 13:57:12 +0200

    vhdl_primitive - delayed programmable circuit - use LUT2s and delay in primitive gates

--- vhdl_primitive/gate_and.vhd
+++ vhdl_primitive/gate_and.vhd
@@ -1,5 +1,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+library UNISIM;
+use UNISIM.vcomponents.all;
 
 entity GATE_AND is
 generic (
@@ -15,3 +17,21 @@ architecture GATE_AND_BEHAVIORAL_1 of GATE_AND is
 begin
 C <= A and B after delay_and;
 end architecture GATE_AND_BEHAVIORAL_1;
+
+architecture GATE_AND_LUT of GATE_AND is
+	signal T : std_logic;
+begin
+-- LUT2: 2-input Look-Up Table with general output
+-- Spartan-3
+-- Xilinx HDL Libraries Guide, version 14.7
+LUT2_inst : LUT2
+generic map (
+	INIT => "1000")
+port map (
+	O	=> T, -- LUT general output
+	I0 => A, -- LUT input
+	I1 => B -- LUT input
+);
+-- End of LUT2_inst instantiation
+C <= T after delay_and;
+end architecture GATE_AND_LUT;
