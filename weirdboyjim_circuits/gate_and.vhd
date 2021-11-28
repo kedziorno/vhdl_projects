f183ef8b6fa8be0a10e43bbafe53f3365b37a3d2
kedziorno <kjoblak85@gmail.com>
2021-11-28 19:23:32 +0100

    wbj - uart - add 74hct32

---
+++ weirdboyjim_circuits/gate_and.vhd
@@ -0,0 +1,37 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+library UNISIM;
+use UNISIM.vcomponents.all;
+
+entity GATE_AND is
+generic (
+delay_and : TIME := 1 ps
+);
+port (
+A,B : in STD_LOGIC;
+C : out STD_LOGIC
+);
+end entity GATE_AND;
+
+architecture GATE_AND_BEHAVIORAL_1 of GATE_AND is
+begin
+C <= A and B after delay_and;
+end architecture GATE_AND_BEHAVIORAL_1;
+
+architecture GATE_AND_LUT of GATE_AND is
+	signal T : std_logic;
+begin
+-- LUT2: 2-input Look-Up Table with general output
+-- Spartan-3
+-- Xilinx HDL Libraries Guide, version 14.7
+LUT2_inst : LUT2
+generic map (
+	INIT => "1000")
+port map (
+	O	=> T, -- LUT general output
+	I0 => A, -- LUT input
+	I1 => B -- LUT input
+);
+-- End of LUT2_inst instantiation
+C <= T after delay_and;
+end architecture GATE_AND_LUT;
---
+++ weirdboyjim_circuits/gate_not.vhd
@@ -0,0 +1,37 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+library UNISIM;
+use UNISIM.vcomponents.all;
+
+entity GATE_NOT is
+generic (
+delay_not : TIME := 1 ps
+);
+port (
+A : in STD_LOGIC;
+B : out STD_LOGIC
+);
+end entity GATE_NOT;
+
+architecture GATE_NOT_BEHAVIORAL_1 of GATE_NOT is
+begin
+B <= not A after delay_not;
+end architecture GATE_NOT_BEHAVIORAL_1;
+
+architecture GATE_NOT_LUT of GATE_NOT is
+	signal T : std_logic;
+begin
+-- LUT2: 2-input Look-Up Table with general output
+-- Spartan-3
+-- Xilinx HDL Libraries Guide, version 14.7
+LUT2_inst : LUT2
+generic map (
+	INIT => "0001")
+port map (
+	O	=> T, -- LUT general output
+	I0 => A, -- LUT input
+	I1 => A -- LUT input
+);
+-- End of LUT2_inst instantiation
+B <= T after delay_not;
+end architecture GATE_NOT_LUT;
