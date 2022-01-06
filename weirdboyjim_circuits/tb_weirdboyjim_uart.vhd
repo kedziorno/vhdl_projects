ffd76839d842e504e823e9ef0786d9f4de3ac5f6
kedziorno <kjoblak85@gmail.com>
2022-01-06 16:21:22 +0100

    wbj - uart - rewrite tb

--- weirdboyjim_circuits/tb_weirdboyjim_uart.vhd
+++ weirdboyjim_circuits/tb_weirdboyjim_uart.vhd
@@ -44,7 +44,6 @@ signal txUartClock : in std_logic;
 signal rxUartClock : in std_logic;
 signal tx : out std_logic;
 signal txData : in std_logic_vector(7 downto 0);
-signal txClock : in std_logic;
 signal TFcount_slv30 : std_logic_vector(3 downto 0);
 signal Rx : in std_logic;
 signal RevData : out std_logic_vector(7 downto 0)
@@ -55,7 +54,6 @@ END COMPONENT;
 signal i_reset : std_logic;
 signal txUartClock,rxUartClock : std_logic := '0';
 signal txData : std_logic_vector(7 downto 0);
-signal txClock : std_logic;
 signal TFcount_slv30 : std_logic_vector(3 downto 0);
 signal Rx : std_logic;
 
@@ -64,10 +62,11 @@ signal tx : std_logic;
 signal RevData : std_logic_vector(7 downto 0);
 
 -- Clock period definitions
+signal uartClock : std_logic;
 constant t : integer := 2**4;
-constant clockPeriod : time := 1 ms;
-constant txUartClock_period : time := clockPeriod;
-constant rxUartClock_period : time := clockPeriod;
+constant uartClockPeriod : time := 0.5 ms;
+constant txUartClock_period : time := uartClockPeriod;
+constant rxUartClock_period : time := uartClockPeriod;
 
 signal tf_flag : std_logic;
 signal tx_run,rx_run : std_logic := '0';
@@ -80,7 +79,6 @@ txUartClock => txUartClock,
 rxUartClock => rxUartClock,
 tx => tx,
 txData => txData,
-txClock => txClock,
 TFcount_slv30 => TFcount_slv30,
 Rx => Rx,
 RevData => RevData
@@ -88,94 +86,136 @@ RevData => RevData
 
 -- Clock process definitions
 
---txUartClock_process : process
+--txUartClock_process : process (uartClock,i_reset,tx_run)
+--	type states is (a,b,c);
+--	variable state : states := a;
 --begin
---	txUartClock <= '0';
---	wait for txUartClock_period/2;
---	txUartClock <= '1';
---	wait for txUartClock_period/2;
---	wait until rx_run = '0';
+--	if (i_reset = '1') then
+--		txUartClock <= '0';
+--	elsif (rising_edge(uartClock)) then
+--		case state is
+--			when a =>
+--				if (tx_run = '1') then
+--					state := b;
+--				end if;
+--			when b =>
+--				txUartClock <= '1';
+--				if (tx_run = '1') then
+--					state := c;
+--				else
+--					state := a;
+--				end if;
+--			when c =>
+--				txUartClock <= '0';
+--				if (tx_run = '1') then
+--					state := b;
+--				else
+--					state := a;
+--				end if;
+--		end case;
+--	end if;
 --end process;
 
---rxUartClock_process : process
+--rxUartClock_process : process (uartClock,rx_run)
+--	type states is (a,b,c);
+--	variable state : states := a;
 --begin
---	rxUartClock <= '0';
---	wait for rxUartClock_period/2;
---	rxUartClock <= '1';
---	wait for rxUartClock_period/2;
---	wait until tx_run = '0';
+--	if (rising_edge(uartClock)) then
+--		case state is
+--			when a =>
+--				if (rx_run = '1') then
+--					state := b;
+--				end if;
+--			when b =>
+--				rxUartClock <= '1';
+--				if (rx_run = '1') then
+--					state := c;
+--				else
+--					state := a;
+--				end if;
+--			when c =>
+--				rxUartClock <= '0';
+--				if (rx_run = '1') then
+--					state := b;
+--				else
+--					state := a;
+--				end if;
+--		end case;
+--	end if;
 --end process;
 
 txUartClock_process : txUartClock <= not txUartClock after txUartClock_period/2 when tx_run = '1' else '0';
 rxUartClock_process : rxUartClock <= not rxUartClock after rxUartClock_period/2 when rx_run = '1' else '0';
 
-txClock_process : process
+uartClock_process : process
 begin
-	txClock <= '0';
-	wait for clockPeriod/2;
-	txClock <= '1';
-	wait for clockPeriod/2;
+	uartClock <= '0';
+	wait for uartClockPeriod/2;
+	uartClock <= '1';
+	wait for uartClockPeriod/2;
 end process;
 
-TFcount_process : process(txClock)
-	variable vtemp : integer range 0 to 15 := 0;
+TFcount_process : process(rxUartClock,txUartClock,i_reset)
+	constant N : integer := t;
+	variable vtemp : integer range 0 to N-1 := 0;
 begin
-	if (rising_edge(txClock)) then
-		if (vtemp = 15) then
+	if (i_reset = '1') then
+		vtemp := 0;
+	elsif (rising_edge(rxUartClock) or rising_edge(txUartClock)) then
+		TFcount_slv30 <= std_logic_vector(to_unsigned(vtemp,4));
+		if (vtemp = N-1) then
 			vtemp := 0;
 		else
 			vtemp := vtemp + 1;
 		end if;
 	end if;
-	TFcount_slv30 <= std_logic_vector(to_unsigned(vtemp,4));
 end process;
 
 tf_flag <= '1' when TFcount_slv30 = "0000" else '0';
 
 -- Stimulus process
 stim_proc: process
-constant N : integer := 7;
+constant N : integer := 9;
 type va is array(integer range <>) of std_logic_vector(7 downto 0);
 constant v : va(0 to N-1) := (
+"11111111",
 "00000000",
 "10101011",
 "11010101",
 "01010100",
 "00101010",
 "11111111",
-"00000000"
+"00000000",
+"11111111"
 );
 begin
 
-tx_run <= 'U';
-rx_run <= 'U';
+i_reset <= '1';
+wait for uartClockPeriod*2;
+i_reset <= '0';
 
-wait for clockPeriod;
-
-tx_run <= '1';
-rx_run <= '0';
-
-wait for clockPeriod;
-
-tx_l0 : for txi in 0 to v'length - 1 loop
-	i_reset <= '1';
-	wait for txUartClock_period;
-	i_reset <= '0';
-	txData <= v(txi);
-	wait for txUartClock_period*256;
-end loop tx_l0;
-
-wait for clockPeriod;
+--tx_run <= '1';
+--rx_run <= '0';
+--
+--wait for uartClockPeriod;
+--
+--tx_l0 : for txi in 0 to v'length - 1 loop
+--	txData <= v(txi);
+--	wait for txUartClock_period*256;
+--	i_reset <= '1';
+--	wait for txUartClock_period;
+--	i_reset <= '0';
+--	wait for txUartClock_period*256;
+--end loop tx_l0;
+--
+--wait for uartClockPeriod;
 
 tx_run <= '0';
 rx_run <= '1';
 
-wait for clockPeriod;
+wait for uartClockPeriod;
 
 rx_l0 : for rxi in 0 to v'length - 1 loop
-	i_reset <= '1';
-	wait for rxUartClock_period;
-	i_reset <= '0';
 	Rx <= '0';
 	wait for rxUartClock_period*t;
 	rx_l1 : for j in 0 to 7 loop
@@ -184,21 +224,20 @@ rx_l0 : for rxi in 0 to v'length - 1 loop
 	end loop rx_l1;
 	Rx <= '1';
 	wait for rxUartClock_period*t;
-	Rx <= 'U';
+	Rx <= '1';
 	wait for rxUartClock_period*t*10;
+--	i_reset <= '1';
+--	wait for rxUartClock_period*2;
+--	i_reset <= '0';
+--	wait for rxUartClock_period*256;
 end loop rx_l0;
 
-wait for clockPeriod;
+wait for uartClockPeriod;
 
 tx_run <= '0';
 rx_run <= '0';
 
-wait for clockPeriod;
-
-tx_run <= 'U';
-rx_run <= 'U';
-
-wait for clockPeriod;
+wait for uartClockPeriod;
 
 -- insert stimulus here
 report "done" severity failure;
