34902cf749c7a9a15c315fc206996733c34e64d1
ko <ko@ko.com>
2022-01-12 21:04:16 +0100

    init commit isim

---
+++ sr/tb_top.vhd
@@ -0,0 +1,78 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+
+USE ieee.numeric_std.ALL;
+
+ENTITY tb_top IS
+END tb_top;
+
+ARCHITECTURE behavior OF tb_top IS 
+
+-- Component Declaration for the Unit Under Test (UUT)
+
+COMPONENT top
+PORT(
+i_clock : IN  std_logic;
+i_reset : IN  std_logic;
+i_push : IN  std_logic;
+i_phase1 : IN  std_logic;
+i_phase2 : IN  std_logic;
+o_cycles : OUT  std_logic_vector(15 downto 0)
+);
+END COMPONENT;
+
+
+--Inputs
+signal i_clock : std_logic := '0';
+signal i_reset : std_logic := '0';
+signal i_push : std_logic := '0';
+signal i_phase1 : std_logic := '0';
+signal i_phase2 : std_logic := '0';
+
+--Outputs
+signal o_cycles : std_logic_vector(15 downto 0);
+
+-- Clock period definitions
+constant i_clock_period : time := 10 ns;
+
+BEGIN
+
+-- Instantiate the Unit Under Test (UUT)
+uut: top PORT MAP (
+i_clock => i_clock,
+i_reset => i_reset,
+i_push => i_push,
+i_phase1 => i_phase1,
+i_phase2 => i_phase2,
+o_cycles => o_cycles
+);
+
+-- Clock process definitions
+i_clock_process :process
+begin
+i_clock <= '0';
+wait for i_clock_period/2;
+i_clock <= '1';
+wait for i_clock_period/2;
+end process;
+
+i_phase2 <= '1' after i_clock_period*20,'0' after i_clock_period*50;
+i_phase1 <= '1' after i_clock_period*15,'0' after i_clock_period*45;
+
+-- Stimulus process
+stim_proc: process
+begin		
+-- hold reset state for 100 ns.
+i_reset <= '1';
+wait for 100 ns;
+i_reset <= '0';
+wait for i_clock_period*5;
+i_push <= '1';
+wait for i_clock_period*1;
+i_push <= '0';
+wait for i_clock_period*150;
+-- insert stimulus here 
+report "done" severity failure;
+end process;
+
+END;
---
+++ sr/top.vhd
@@ -0,0 +1,145 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+use IEEE.NUMERIC_STD.ALL;
+
+library UNISIM;
+use UNISIM.VComponents.all;
+
+entity top is
+port (
+	signal i_clock : in std_logic;
+	signal i_reset : in std_logic;
+	signal i_push : in std_logic;
+	signal i_phase1 : in std_logic;
+	signal i_phase2 : in std_logic;
+	signal o_cycles : out std_logic_vector(15 downto 0)
+);
+end top;
+
+architecture Behavioral of top is
+
+signal counter : integer range 0 to 2**16-1;
+signal enable : std_logic;
+signal tick1,tick2 : std_logic;
+
+begin
+
+-- phase detector
+--FDCPE_inst1 : FDCPE generic map (INIT => '0')
+--port map (Q => fd1q, C => i_phase1, CE => '1', CLR => ando2, D => '1', PRE => '0');
+--FDCPE_inst2 : FDCPE generic map (INIT => '0')
+--port map (Q => fd2q, C => i_phase2, CE => '1', CLR => ando2, D => '1', PRE => '0');
+--MULT_AND_inst : MULT_AND
+--port map (LO => ando1, I0 => fd1q, I1 => fd2q);
+--ando2 <= ando1 after 1 ns;
+--o_cycles <= fd1q;
+
+pre1 : process (i_clock,i_reset,i_phase1) is
+	type states is (a,b,c);
+	variable state : states;
+begin
+	if (i_reset = '1') then
+		tick1 <= '0';
+		state := a;
+	elsif (rising_edge(i_clock)) then
+		case (state) is
+			when a =>
+				if (i_phase1 = '1') then
+					state := b;
+					tick1 <= '1';
+				else
+					state := a;
+					tick1 <= '0';
+				end if;
+			when b =>
+				state := b;
+				tick1 <= '0';
+			when others =>
+				state := a;
+				tick1 <= '0';
+		end case;		
+	end if;
+end process pre1;
+
+pre2 : process (i_clock,i_reset,i_phase2) is
+	type states is (a,b,c);
+	variable state : states;
+begin
+	if (i_reset = '1') then
+		tick2 <= '0';
+		state := a;
+	elsif (rising_edge(i_clock)) then
+		case (state) is
+			when a =>
+				if (i_phase2 = '1') then
+					state := b;
+					tick2 <= '1';
+				else
+					state := a;
+					tick2 <= '0';
+				end if;
+			when b =>
+				state := b;
+				tick2 <= '0';
+			when others =>
+				state := a;
+				tick2 <= '0';
+		end case;		
+	end if;
+end process pre2;
+
+pcnt : process (i_clock,i_reset,enable) is
+begin
+	if (i_reset = '1') then
+		counter <= 0;
+	elsif (rising_edge(i_clock)) then
+		if (enable = '1') then
+			counter <= counter + 1;
+		else
+			counter <= counter;
+		end if;
+	end if;
+end process pcnt;
+
+p0 : process (i_clock,i_reset,tick1,tick2) is
+	type states is (a,b,c,d);
+	variable state : states;
+begin
+	if (i_reset = '1') then
+		state := a;
+		enable <= '0';
+		o_cycles <= (others => '0');
+	elsif(rising_edge(i_clock)) then
+		case (state) is
+			when a =>
+				if (i_push = '1') then
+					state := b;
+				else
+					state := a;
+				end if;
+			when b =>
+				if (tick1 = '1') then
+					enable <= '1';
+					state := c;
+				else
+					enable <= '0';
+					state := a;
+				end if;
+			when c =>
+				if (tick2 = '1') then
+					enable <= '0';
+					state := d;
+				else
+					enable <= '1';
+					state := c;
+				end if;
+			when d =>
+				o_cycles <= std_logic_vector(to_unsigned(counter,16));
+				state := a;
+		end case;
+	end if;
+end process p0;
+
+end Behavioral;
+
