348f1e0fec62c97cafc7fca21aaa17b9855f6b3f
kedziorno <kjoblak85@gmail.com>
2024-12-04 16:58:21 +0100

    vhdl_primitive - revert files

--- vhdl_primitive/transmission_gate.vhd
+++ vhdl_primitive/transmission_gate.vhd
@@ -31,13 +31,13 @@ use UNISIM.VComponents.all;
 
 entity transmission_gate is
 port (
-	io_a : in  std_logic;
-	io_b : out std_logic;
+	io_a : inout std_logic;
+	io_b : inout std_logic;
 	i_s : in std_logic
 );
 end transmission_gate;
 
-architecture Behavioral1 of transmission_gate is
+architecture Behavioral of transmission_gate is
 
 	signal s,sb,t1,t2 : std_logic;
 
@@ -70,31 +70,10 @@ begin
 s <= i_s;
 GNOT : GATE_NOT GENERIC MAP (1 ns) PORT MAP (A=>s,B=>sb);
 
---IOBUF_inst1 : IOBUF port map (O=>t1,IO=>io_a,I=>t2,T=>s);
---IOBUF_inst2 : IOBUF port map (O=>t2,IO=>io_b,I=>t1,T=>sb);
+IOBUF_inst1 : IOBUF
+port map (O=>t1,IO=>io_a,I=>t2,T=>s);
 
-end Behavioral1;
+IOBUF_inst2 : IOBUF
+port map (O=>t2,IO=>io_b,I=>t1,T=>sb);
 
-architecture Behavioral2 of transmission_gate is
-
-signal s,sb : std_logic;
-
-component GATE_NOT is
-generic (
-delay_not : TIME := 1 ns
-);
-port (
-A : in STD_LOGIC;
-B : out STD_LOGIC
-);
-end component GATE_NOT;
-
-begin
-
-s <= i_s;
-GNOT : GATE_NOT GENERIC MAP (1 ns) PORT MAP (A=>s,B=>sb);
-
-IOBUF_inst1 : io_b <= io_a when s = '1' else 'Z';
---IOBUF_inst2 : io_a <= io_b after 1 ns when sb = '1' else 'Z';
-
-end Behavioral2;
+end Behavioral;
